68367b6d0c2e5a631dd615b790452e3a044ffeda
coldtema <coldtema1703@gmail.com>
2025-02-24 14:03:22 +0300

    grouped urls in patterns + added parameters of url - line into view-func 'about'

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,9 +1,29 @@
 import apps.blog.views as views
-from django.urls import path, re_path
+from django.urls import path, re_path, include
+
+
+about_patterns = [
+    path(r'<str:name>/<int:age>', views.about, name ='about_mod'),
+    path(r'', views.about, name ='about_def'),
+]
+
+product_detailed_patterns = [
+    path('', views.products_def, name='products_def_id'),
+    path('questions/', views.products_def_questions, name='products_def_questions'),
+    path('comments/', views.products_def_comments, name='products_def_comments'),
+]
+
+product_patterns = [
+    path('new/', views.products_new, name='products_new'),
+    path('top/', views.products_top, name='products_top'),
+    path('', views.products_def, name='products_def'),
+    path('<int:id>/', include(product_detailed_patterns))
+]
 
 urlpatterns = [
     path('', views.index, name='hello'), #путь, функция представления, и имя пути для лаконичности
-    path(r'about/<str:name>/<int:age>', views.about, name ='about_mod'),
-    path(r'about/', views.about, name ='about_def'),
-    path('contacts', views.contacts, name='contacts')
+    path(r'about/', include(about_patterns)),
+    path('products/', include(product_patterns)),
+    # path('message/<str:category>/<str:subcategory>/<str:theme>/<int:number>', views.message, name = 'message'),
+    re_path(r'^message/(?P<category>[a-z]+?)/(?P<subcategory>\D+?)/(?P<theme>\D+?)/(?P<number>\d+)', views.message, name = 'message')
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -3,15 +3,64 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h2>Главная</h2>')
+    print(request)
+    return HttpResponse('<h2>Hello in app BLOG</h2>')
+
+
+def accounts(request, user_name = 'NoName'):
+    return HttpResponse(f'Имя аккаунта: {user_name}')
+
 
 
 def about(request, name = '??', age='??'):
+    print(request.GET)
+    if name == '??' and age == '??':
+        return HttpResponse(f'''<h2>О сайте</h2>
+                    <h2>name: {request.GET.get('name', 'Не передал имени, амебус')}</h2>
+                    <h2>age: {request.GET.get('age', 'Не передал возраста, 0 лет')}</h2>
+                    ''')   # альтернативно -> request.GET['name'] - но будет выдавать ошибку, если не найдет нужный параметр
     return HttpResponse(f'''<h2>О сайте</h2>
                         <h2>name: {name}</h2>
                         <h2>age: {age}</h2>
                         ''')
 
 
-def contacts(request):
-    return HttpResponse('<h2>Контакты</h2>')
+
+
+def contacts(request, phone_number):
+    return HttpResponse(f'<h2>Контакты: {phone_number}</h2>')
+
+
+
+
+
+
+def products_new(request):
+    return HttpResponse("<h1>Новые продукты</h1>")
+
+
+def products_top(request):
+    return HttpResponse("<h1>Топ продуктов</h1>")
+
+def products_def(request, id=None):
+    if id is None:
+        return HttpResponse("<h1>Главная страница в продуктах блога</h1>")
+    return HttpResponse(f"<h1>Продукт {id}</h1>")
+
+def products_def_questions(request, id):
+    return HttpResponse(f"<h1>Вопросы о продукте {id}</h1>")
+
+def products_def_comments(request, id):
+    return HttpResponse(f"<h1>Комментарии о продукте {id}</h1>")
+
+
+
+
+def message(request, category, subcategory, theme, number):
+    return HttpResponse(f'''<ul>
+<li>Категория: {category}</li>
+<li>Подкатегория: {subcategory}</li>
+<li>Тема: {theme}</li>
+<li>Номер: {number}</li>
+</ul>
+ ''')
