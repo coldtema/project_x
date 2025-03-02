f2d497e35f2c81e2a8c640b55892dead0fc05db2
coldtema <coldtema1703@gmail.com>
2025-03-02 21:28:15 +0300

    added UserEditForm for reg-edit + templae for editing users

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -5,3 +5,8 @@ class UserForm(forms.Form):
     age = forms.IntegerField(max_value=100, min_value=0, label='Введите ваш возраст')
     # prof_author = forms.NullBooleanField(label='Профессиональный автор', required=True, help_text='gfb')
     # password = forms.CharField(widget=forms.widgets.PasswordInput)
+
+class UserEditForm(forms.Form):
+    nickname = forms.CharField(max_length=20, label='Исправьте никнейм')
+    age = forms.IntegerField(max_value=100, min_value=0, label='Исправьте возраст')
+    id = forms.IntegerField(widget = forms.HiddenInput())
