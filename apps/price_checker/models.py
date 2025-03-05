df90c9129f847a0542ed7bbb9014a4689347c4c7
coldtema <coldtema1703@gmail.com>
2025-03-06 01:35:07 +0300

    deleted old_price in models (will be sepereate table) + migrated

--- apps/price_checker/models.py
+++ apps/price_checker/models.py
@@ -4,8 +4,7 @@ from apps.blog.models import Author
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
-    old_price = models.IntegerField()
     url = models.URLField()
-    image = models.URLField()
+    image = models.URLField(null=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='products')
 
