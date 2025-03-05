0472117e746a4b0628aea21085ac4dc3a5f118c6
coldtema <coldtema1703@gmail.com>
2025-03-05 23:13:00 +0300

    made functions for understanding, what shop is to go in link + parsing data from requests of each shop (funcs)

---
+++ apps/price_checker/site_explorer.py
@@ -0,0 +1,88 @@
+from requests import request
+from bs4 import BeautifulSoup
+import re
+
+
+def get_shop_of_product(product_url):
+    regex = r'://(www.)?([\w-]+).(\w+)/'
+    return shop_to_func.get(re.search(pattern=regex, string=product_url).group(2))(product_url)
+
+    
+def get_product_brandshop(product_url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = request('GET', url=product_url ,headers=headers)
+    soup_engine = BeautifulSoup(response.text, 'html.parser')
+    price_element = soup_engine.find("div", class_="product-order__price_new").text.strip()
+    brand = soup_engine.find("div", class_="product-page__header font font_title-l").text.strip()
+    category, model = map(lambda x: x.text.strip(), soup_engine.find_all("div", class_="product-page__subheader font font_m font_grey"))
+    return {'price_element': price_element, 'brand': brand, 'model': model, 'category': category}
+
+
+def get_product_rendez_vous(product_url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = request('GET', url=product_url, headers=headers)
+    soup_engine = BeautifulSoup(response.text, 'html.parser')
+    price_element = soup_engine.find("span", class_="item-price-value").text.strip()
+    category_plus_brand = soup_engine.find("span", class_="item-name-title").text.strip()
+    brand = ''
+    category = ''
+    for elem in category_plus_brand.split():
+        if elem.upper() == elem:
+            brand += f' {elem}'
+        else:
+            category += f' {elem}'
+    brand = brand.strip()
+    category = category.strip()
+    return {'price_element': price_element, 'brand': brand, 'model': 'model', 'category': category}
+
+
+def get_product_tsum(product_url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = request('GET', url=product_url, headers=headers)
+    soup_engine = BeautifulSoup(response.text, 'html.parser')
+    price_element = soup_engine.find("p", class_=re.compile(r'Price__price___\w+')).text.strip()
+    brand = soup_engine.find("div", class_=re.compile(r'ColumnView__infoBlocksRow___\w+')).text.strip()
+    brand = re.search(pattern=r'Бренд: (.+?)С', string=brand).group(1)
+    category = soup_engine.find("h1", class_=re.compile(r'description__productName___\w+')).text.strip()
+    category = re.search(pattern=r'[А-Я].+', string=category).group(0)
+    return {'price_element': price_element, 'brand': brand, 'model': 'model', 'category': category}
+
+
+def get_product_lamoda(product_url):
+    return 'poka delaetsya'
+#     headers = {"User-Agent": "Mozilla/5.0"}
+#     response = request('GET', url=product_url ,headers=headers)
+#     soup_engine = BeautifulSoup(response.text, 'html.parser')
+#     print(soup_engine)
+#     price_element = soup_engine.find("div", class_=re.compile(r'_title_.+')).text.strip()
+#     print(price_element)
+#     # brand = soup_engine.find("div", class_=re.compile(r'ColumnView__infoBlocksRow___\w+')).text.strip()
+#     # brand = re.search(pattern=r'Бренд: (.+?)С', string=brand).group(1)
+#     # category = soup_engine.find("h1", class_=re.compile(r'description__productName___\w+')).text.strip()
+#     # category = re.search(pattern=r'[А-Я].+', string=category).group(0)
+#     # return {'price_element': price_element, 'brand': brand, 'model': 'model', 'category': category}
+
+
+
+def get_product_street_beat(product_url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = request('GET', url=product_url ,headers=headers)
+    soup_engine = BeautifulSoup(response.text, 'html.parser')
+    price_element = soup_engine.find("div", class_="price-tag").text.strip()
+    print(price_element)
+    # brand = soup_engine.find("div", class_="product-page__header font font_title-l").text.strip()
+    # category, model = map(lambda x: x.text.strip(), soup_engine.find_all("div", class_="product-page__subheader font font_m font_grey"))
+    # return {'price_element': price_element, 'brand': brand, 'model': model, 'category': category}
+
+shop_to_func = {'brandshop': get_product_brandshop, 'rendez-vous': get_product_rendez_vous, 'tsum': get_product_tsum, 'lamoda': get_product_lamoda, 'street-beat': get_product_street_beat}
+
+
+
+
+
+# ненужная регулярка для вычленения цены, тк bs4 сам это делает при переводе .text
+# price_digger = re.search(pattern=r'[\d ]+ ₽', string='''<div class="product-order__price_new">
+#     3 450 ₽
+#   </div>''')
+# print(price_digger.group(0).strip())
+
