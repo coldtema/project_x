1ead7f07badbcd965346d1482b10ed937addae3e
coldtema <coldtema1703@gmail.com>
2025-03-06 01:37:19 +0300

    made a creation of object in db + modified template to see this interactively

--- apps/price_checker/views.py
+++ apps/price_checker/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from .forms import ProductForm
 from .models import Product
 from .site_explorer import get_product_brandshop, get_shop_of_product
-import time
+from apps.blog.models import Author
 
 def all_price_list(request):
     if request.method == 'POST':
         product_form = ProductForm(request.POST)
-        print(request.POST)
         if product_form.is_valid():
             product_data = get_shop_of_product(request.POST.get('url'))
-            return render(request, 'price_checker/temp_view_prod.html', context={'product_data': product_data})
+            Product.objects.create(name=product_data['name'], price=product_data['price_element'], url=request.POST.get('url'), author=Author.objects.get(id=2))
+            return HttpResponseRedirect('/price_checker')
         else:
             return HttpResponseBadRequest('Так себе ссылка')
     else:
         product_form = ProductForm()
-        return render(request, 'price_checker/index.html', context={'form': product_form})
+        db_products = Product.objects.all()
+        return render(request, 'price_checker/index.html', context={'form': product_form, 'db_products': db_products})
