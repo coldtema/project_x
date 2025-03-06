1b59f851fa2e5e71b521b1f8f7e268c1e47354b3
coldtema <coldtema1703@gmail.com>
2025-03-06 13:21:48 +0300

    made new table for history of a price + added script in view func for creating new object in this table

--- apps/price_checker/models.py
+++ apps/price_checker/models.py
@@ -3,8 +3,13 @@ from apps.blog.models import Author
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.IntegerField()
+    latest_price = models.IntegerField()
     url = models.URLField()
     image = models.URLField(null=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='products')
+
+class Price(models.Model):
+    price = models.IntegerField()
+    added_time = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='price')
 
--- apps/price_checker/views.py
+++ apps/price_checker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from .forms import ProductForm
-from .models import Product
+from .models import Product, Price
 from .site_explorer import get_product_brandshop, get_shop_of_product
 from apps.blog.models import Author
 
@@ -10,7 +10,8 @@ def all_price_list(request):
         product_form = ProductForm(request.POST)
         if product_form.is_valid():
             product_data = get_shop_of_product(request.POST.get('url'))
-            Product.objects.create(name=product_data['name'], price=product_data['price_element'], url=request.POST.get('url'), author=Author.objects.get(id=2))
+            new_product = Product.objects.create(name=product_data['name'], latest_price=product_data['price_element'], url=request.POST.get('url'), author=Author.objects.get(id=2))
+            Price.objects.create(product=new_product, price=product_data['price_element'])
             return HttpResponseRedirect('/price_checker')
         else:
             return HttpResponseBadRequest('Так себе ссылка')
