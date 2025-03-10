b3881e5483d7ed32f7d4386aee9e3c17f8bf764e
coldtema <coldtema1703@gmail.com>
2025-03-11 02:07:06 +0300

    made new Test object not to save in order to validation in chat-models (doubling) + sending it in tg_client

--- apps/test_generator/views.py
+++ apps/test_generator/views.py
@@ -3,16 +3,14 @@ from django.http import HttpResponseRedirect, HttpResponseBadRequest
 from .forms import AddTest
 from .models import Test
 from .tg_client import send_message_and_get_reply
-from .question_digger import claim_questions
 
 def all_tests_list(request):
     all_tests = Test.objects.all()
     if request.method == 'POST':
         form = AddTest(request.POST)
         if form.is_valid():
-            new_test = Test.objects.create(name=request.POST.get('name'), text=request.POST.get('text'))
-            response_from_bot = send_message_and_get_reply(request.POST.get('text'))
-            claim_questions(test_object = new_test, text=response_from_bot)
+            new_test = Test(name=request.POST.get('name'), text=request.POST.get('text'))
+            send_message_and_get_reply(request.POST.get('text'), new_test)
             return HttpResponseRedirect('/test_generator')
     else:
         return render(request, 'test_generator/all_tests.html', context={'form': AddTest, 'all_tests': all_tests})
