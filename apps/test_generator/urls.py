f1aa5fb370d8cf23e0e778238c35d3c1fe19af27
coldtema <coldtema1703@gmail.com>
2025-03-10 04:24:14 +0300

    added url for func pass test

--- apps/test_generator/urls.py
+++ apps/test_generator/urls.py
@@ -4,4 +4,5 @@ from apps.test_generator import views
 
 urlpatterns = [
     path('', views.all_tests_list, name='test_generator'),
+    path('pass_test/<int:id>', views.pass_test, name='pass_test')
 ]
