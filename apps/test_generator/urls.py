dcc1576ff67eb8f0bb79c30918640dc516ecd27e
coldtema <coldtema1703@gmail.com>
2025-03-11 03:00:11 +0300

    added button - deletion of a test

--- apps/test_generator/urls.py
+++ apps/test_generator/urls.py
@@ -4,5 +4,6 @@ from apps.test_generator import views
 
 urlpatterns = [
     path('', views.all_tests_list, name='test_generator'),
-    path('pass_test/<int:id>', views.pass_test, name='pass_test')
+    path('pass_test/<int:id>', views.pass_test, name='pass_test'),
+    path('delete_test/<int:id>', views.delete_test, name='delete_test'),
 ]
