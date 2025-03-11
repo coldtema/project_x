1879c8fa8a8b7bde10420b2a965b1c67d9437c93
coldtema <coldtema1703@gmail.com>
2025-03-11 15:55:18 +0300

    added url for the page of viewing answers on the particular test

--- apps/test_generator/urls.py
+++ apps/test_generator/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.all_tests_list, name='test_generator'),
     path('pass_test/<int:id>', views.pass_test, name='pass_test'),
     path('delete_test/<int:id>', views.delete_test, name='delete_test'),
+    path('pass_test/<int:id>/answers/', views.answers_on_test, name='answers_on_test'),
 ]
