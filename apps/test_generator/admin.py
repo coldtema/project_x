8d4db35b15b77d9f7b1ef5e99b5a96fdaa31e5a9
coldtema <coldtema1703@gmail.com>
2025-03-13 02:53:54 +0300

    registered models from test_generator in admin-panel

--- apps/test_generator/admin.py
+++ apps/test_generator/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Test, Question
 
 
-admin.site.register(Test)
-admin.site.register(Question)
+#admin.site.register(Test)
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ['name', 'added', 'best_result']
+    list_filter = ['name']
+    search_fields = ['name', 'text']
+
+
+@admin.register(Question)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ['text', 'answer1', 'answer2', 'answer3', 'right_answer', 'test__name'] 
+    list_filter = ['test__name']
+    search_fields = ['name', 'text', 'test__name']
+    ordering = ['-test__name']
