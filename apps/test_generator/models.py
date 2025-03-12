6f16b4df93c29cd8636be32e19eeb046c1f2815d
coldtema <coldtema1703@gmail.com>
2025-03-12 23:10:24 +0300

    made better names in models to see them in admin panel

--- apps/test_generator/models.py
+++ apps/test_generator/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 
 class Test(models.Model):
-    name = models.CharField(max_length=200)
-    text = models.TextField()
-    added = models.DateTimeField(auto_now_add=True)
-    best_result = models.IntegerField(default=0)
+    name = models.CharField(max_length=200, verbose_name='Имя теста')
+    text = models.TextField(verbose_name='Текст теста')
+    added = models.DateTimeField(auto_now_add=True, verbose_name='Имя теста')
+    best_result = models.IntegerField(default=0, verbose_name='Лучший результат')
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
 
     def __str__(self):
         return f'{self.text[:20]}...'
 
 
 class Question(models.Model):
-    text = models.TextField()
-    answer1 = models.CharField(max_length=2000, null=True)
-    answer2 = models.CharField(max_length=2000, null=True)
-    answer3 = models.CharField(max_length=2000, null=True)
-    right_answer = models.CharField(max_length=10, null=True)
+    text = models.TextField(verbose_name='Текст вопроса')
+    answer1 = models.CharField(max_length=2000, null=True, verbose_name='Ответ 1')
+    answer2 = models.CharField(max_length=2000, null=True, verbose_name='Ответ 2')
+    answer3 = models.CharField(max_length=2000, null=True, verbose_name='Ответ 3')
+    right_answer = models.CharField(max_length=10, null=True, verbose_name='Правильный ответ')
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{' '.join(self.text.split()[:3])}...'
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
