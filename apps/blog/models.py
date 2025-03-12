5863e2362b7af967ba0eedbdec3bc24e3d75bf29
coldtema <coldtema1703@gmail.com>
2025-03-13 02:52:50 +0300

    some meta changes for model author for the better view

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 class Author(models.Model):
-    nickname = models.CharField(max_length=20)
-    age = models.IntegerField(default=0)
+    nickname = models.CharField(max_length=20, verbose_name='Имя')
+    age = models.IntegerField(default=0, verbose_name='Возраст')
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ['nickname']
 
     def __str__(self):
         return self.nickname
