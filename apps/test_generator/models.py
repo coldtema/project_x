99fea68be769b3a4b92e8d527a326e48283b42de
coldtema <coldtema1703@gmail.com>
2025-03-13 02:54:28 +0300

    some changes in models of test_generator

--- apps/test_generator/models.py
+++ apps/test_generator/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Test(models.Model):
     name = models.CharField(max_length=200, verbose_name='Имя теста')
     text = models.TextField(verbose_name='Текст теста')
-    added = models.DateTimeField(auto_now_add=True, verbose_name='Имя теста')
+    added = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
     best_result = models.IntegerField(default=0, verbose_name='Лучший результат')
 
     class Meta:
@@ -20,10 +20,12 @@ class Question(models.Model):
     answer2 = models.CharField(max_length=2000, null=True, verbose_name='Ответ 2')
     answer3 = models.CharField(max_length=2000, null=True, verbose_name='Ответ 3')
     right_answer = models.CharField(max_length=10, null=True, verbose_name='Правильный ответ')
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, verbose_name='Имя теста')
 
-    def __str__(self):
-        return f'{' '.join(self.text.split()[:3])}...'
     class Meta:
         verbose_name = 'Вопрос'
         verbose_name_plural = 'Вопросы'
+
+    def __str__(self):
+        return f'{' '.join(self.text.split()[:3])}...'
+    
