4af4e509f49e81fab947647d694517e5624eafef
coldtema <coldtema1703@gmail.com>
2025-03-12 22:47:58 +0300

    added new field max_questions for generating questions in all models

--- apps/test_generator/forms.py
+++ apps/test_generator/forms.py
@@ -4,6 +4,7 @@ from .models import Test
 class AddTest(forms.Form):
     name = forms.CharField(max_length=100, label='Имя теста')
     text = forms.CharField(widget=forms.Textarea, label='Текст для анализа')
+    max_questions = forms.BooleanField(label='Максимум вопросов (энергозатратно)', required=False)
     class Meta:
         model = Test
         fields = ['name', 'text']
