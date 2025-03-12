1109c14f607368692857eeea761c9eb1de8206ae
coldtema <coldtema1703@gmail.com>
2025-03-13 02:42:55 +0300

    added admin models in admin panel

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Post, Author
 
-admin.site.register(Post) #site - это объект представляющий собой административный сайт в django
-admin.site.register(Author)
+# admin.site.register(Post) site - это объект представляющий собой административный сайт в django
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','slug', 'description', 'text', 'created', 'updated', 'fixed', 'author__nickname']
+    list_filter = ['title', 'author__nickname', 'created', 'fixed']
+    prepopulated_fields = {'slug': ('title', )}
+    search_fields = ['title', 'author__nickname'] #обязательно указываем поле, по которому делаем серч
+    ordering = ['-created']
+    # raw_id_fields = ['author'] - круче встроенный
+
+
+
+@admin.register(Author)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['nickname', 'age']
+    list_filter = ['nickname', 'age']
+    search_fields = ['nickname']
+    ordering = ['-age']
+    # raw_id_fields = ['author'] - круче встроенный
