a32633828a500584b3a96ea67b9746730a3841c1
coldtema <coldtema1703@gmail.com>
2025-03-15 00:28:32 +0300

    changed verbose_name in model author in app blog

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Author(models.Model):
-    nickname = models.CharField(max_length=20, verbose_name='Имя')
+    nickname = models.CharField(max_length=20, verbose_name='Никнейм')
     age = models.IntegerField(default=0, verbose_name='Возраст')
 
     class Meta:
