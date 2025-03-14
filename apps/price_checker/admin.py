90df0d0542a27761165b49301367ef07f10e5c34
coldtema <coldtema1703@gmail.com>
2025-03-15 00:29:20 +0300

    added verbose names in models of price_checker an registered them in admin-panel

--- apps/price_checker/admin.py
+++ apps/price_checker/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Price
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'shop', 'latest_price', 'url', 'image', 'author']
+    list_filter = ['shop', 'author__nickname']
+    search_fields = ['name']
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ['price', 'added_time', 'product']
+    list_filter = ['product__name']
+    search_fields = ['product__name']
+    ordering = ['price']
--- apps/price_checker/models.py
+++ apps/price_checker/models.py
@@ -2,18 +2,26 @@ from django.db import models
 from apps.blog.models import Author
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    shop = models.CharField(max_length=100, blank=True)
-    latest_price = models.IntegerField()
-    url = models.URLField()
-    image = models.URLField(null=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=100, verbose_name='Имя продукта')
+    shop = models.CharField(max_length=100, blank=True, verbose_name='Магазин')
+    latest_price = models.IntegerField(verbose_name='Последняя цена')
+    url = models.URLField(verbose_name='URL')
+    image = models.URLField(null=True, verbose_name='Картинка')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='products', verbose_name='Никнейм автора')
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+    
+    def __str__(self):
+        return self.name
 
 class Price(models.Model):
-    price = models.IntegerField()
-    added_time = models.DateTimeField(auto_now_add=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.IntegerField(verbose_name='Цена')
+    added_time = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Имя продукта')
+
     class Meta:
         ordering = ['added_time']
-
+        verbose_name = 'Цена'
+        verbose_name_plural = 'Цены'
 
