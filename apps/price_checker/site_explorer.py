1c1078175d0f96476cb53cae9a25d7e12c27c6c7
coldtema <coldtema1703@gmail.com>
2025-03-15 03:02:24 +0300

    added price_digger func for lacoste

--- apps/price_checker/site_explorer.py
+++ apps/price_checker/site_explorer.py
@@ -7,7 +7,7 @@ import json
 def get_shop_of_product(product_url):
     '''Функция, определяющая, какому магазину принадлежит ссылка'''
     regex = r'://(www.)?([\w-]+).(\w+)/'
-    return shop_to_func.get(re.search(pattern=regex, string=product_url).group(2))(product_url)
+    return shop_to_func.get(re.search(pattern=regex, string=product_url).group(2).strip())(product_url)
 
     
 
@@ -38,7 +38,7 @@ def get_product_superstep(product_url):
         price_element = soup_engine.find("div", class_="price").text.strip()
         price_element = price_element.split('₽')[0]
     price_element = int(''.join(list(filter(lambda x: True if x.isdigit() else False, price_element.split()))))
-    name = soup_engine.find("div", class_="detail__info-wrapper") #.split('\n')[0].strip()
+    name = soup_engine.find("div", class_="detail__info-wrapper")
     name = ' '.join(list(name.stripped_strings)) #переделанный в строку генератор отредактированных строк
     name = re.search(pattern=r'(.+?) Цвет', string=name).group(1)
     return {'price_element': price_element, 'name': name, 'shop': 'superstep'}
@@ -81,14 +81,6 @@ def get_product_tsum(product_url):
 
 
 
-def get_product_lamoda(product_url):
-    '''Функция для парсинга товара из lamoda'''
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = request('GET', url=product_url ,headers=headers)
-    return {'price_element': 6000, 'name': 'Кофта'}
-
-
-
 def get_product_street_beat(product_url):
     '''Функция для парсинга товара из street beat'''
     headers = {"User-Agent": "Mozilla/5.0"}
@@ -100,13 +92,59 @@ def get_product_street_beat(product_url):
     return {'price_element': price_element, 'name': name, 'shop': 'street-beat'}
 
 
+def get_product_lacoste(product_url):
+    '''Функция для парсинга товара из superstep'a'''
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = request('GET', url=product_url, headers=headers)
+    soup_engine = BeautifulSoup(response.text, 'html.parser')
+    price_element = soup_engine.find("div", class_="nl-product-price nl-product-configuration__price").text.strip().split('₽')[0]
+    price_element = int(''.join(list(filter(lambda x: True if x.isdigit() else False, price_element.split()))))
+    name = soup_engine.find("h4", class_="nl-product-configuration__title").text.strip()
+    return {'price_element': price_element, 'name': name, 'shop': 'lacoste'}
 
-def get_product_ozon(product_url):
+
+
+def get_product_lamoda(product_url): #блокает
+    '''Функция для парсинга товара из lamoda'''
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = request('GET', url=product_url ,headers=headers)
+    return {'price_element': 6000, 'name': 'Кофта'}
+
+
+
+def get_product_lgcity(product_url): #блокает
+    '''Функция для парсинга товара из lady & gentleman city'''
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://google.com",
+    "Accept-Encoding": "gzip, deflate, br"
+}
+    response = request('GET', url=product_url, headers=headers)
+    soup_engine = BeautifulSoup(response.text, 'html.parser')
+    print(soup_engine.text)
+    try:
+        price_element = soup_engine.find("div", class_="card__info-price-text").text.strip()
+    except:
+        price_element = soup_engine.find("div", class_="card__info-price-text card__info-price-text--new").text.strip()
+    # price_element = int(''.join(list(filter(lambda x: True if x.isdigit() else False, price_element.split()))))
+    print('зашел')
+    print(price_element)
+    return
+    name = soup_engine.find("div", class_="detail__info-wrapper") #.split('\n')[0].strip()
+    name = ' '.join(list(name.stripped_strings)) #переделанный в строку генератор отредактированных строк
+    name = re.search(pattern=r'(.+?) Цвет', string=name).group(1)
+    return {'price_element': price_element, 'name': name, 'shop': 'lady & gentleman city'}
+
+
+
+def get_product_ozon(product_url): #блокает
     ...
 
 
 
-def get_product_sportmaster(product_url):
+def get_product_sportmaster(product_url): #блокает
     headers = {"User-Agent": "Mozilla/5.0"}
     response = request('GET', url=product_url, headers=headers)
     print(response.text)
@@ -120,7 +158,9 @@ shop_to_func = {'brandshop': get_product_brandshop,
                 'street-beat': get_product_street_beat,
                 'ozon': get_product_ozon,
                 'sportmaster': get_product_sportmaster,
-                'superstep': get_product_superstep}
+                'superstep': get_product_superstep,
+                'lgcity': get_product_lgcity,
+                'lacoste': get_product_lacoste}
 
 
 
