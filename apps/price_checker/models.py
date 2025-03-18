e0a4fdd81f43ec17bf37a54bf0da15f506ca8713
coldtema <coldtema1703@gmail.com>
2025-03-18 15:16:31 +0300

    1. added field created and updated in model product 2. changed ordering for updated 3. made values for these fields according to fields in table price 4. overrided func of sving element in nodel in order to chnge field updated only if field latest_price is changed

--- apps/price_checker/models.py
+++ apps/price_checker/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.blog.models import Author
+from django.utils import timezone
 
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя продукта')
@@ -7,16 +8,26 @@ class Product(models.Model):
     category = models.CharField(max_length=100, verbose_name='Категория', blank=True)
     latest_price = models.IntegerField(verbose_name='Последняя цена')
     url = models.URLField(verbose_name='URL')
-    image = models.URLField(null=True, verbose_name='Картинка')
     enabled = models.BooleanField(default=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='products', verbose_name='Никнейм автора')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления продукта')
+    updated = models.DateTimeField(default=timezone.now, verbose_name='Время обновления продукта')
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-        ordering = ['-id']
+        ordering = ['-updated']
     
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if self.pk:  # Проверяем, что объект уже существует (не новый)
+            original = Product.objects.get(pk=self.pk)
+            if self.latest_price != original.latest_price:  # Проверяем, изменилось ли нужное поле
+                self.updated = timezone.now()  # Обновляем вручную
+            else:
+                self.updated = original.updated
+        super().save(*args, **kwargs)
     
 
 class Price(models.Model):
