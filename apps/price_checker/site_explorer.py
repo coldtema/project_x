9a7f3c534ef859fe3cb1ce68cad808c553da7aed
coldtema <coldtema1703@gmail.com>
2025-03-21 14:43:12 +0300

    added parser for bebakids

--- apps/price_checker/site_explorer.py
+++ apps/price_checker/site_explorer.py
@@ -2426,6 +2426,20 @@ def get_product_birota(product_url):
 
 
 
+def get_product_bebakids(product_url):
+    '''Функция для парсинга товара из bebakids'''
+    headers = {"User-Agent": "Mozilla/5.0"}
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(product_url, headers=headers)
+    soup_engine = BeautifulSoup(response.text, 'lxml')
+    price_element = soup_engine.find('div', class_='wr_item_3_1').text.strip()
+    price_element = price_element.split('₽')[0]
+    price_element = int(''.join(list(filter(lambda x: True if x.isdigit() else False, price_element))))
+    name = soup_engine.find('h1').text.strip()
+    return {'price_element': price_element, 'name': name, 'shop': 'bebakids', 'category': shop_to_category['bebakids']}
+
+
+
 
 
 
@@ -2745,7 +2759,7 @@ shop_to_func = {'brandshop': get_product_brandshop,
                 'vamvelosiped': get_product_vamvelosiped,
                 'book24': get_product_book24,
                 'birota': get_product_birota,
-                # 'bebakids': get_product_bebakids,
+                'bebakids': get_product_bebakids,
                 # 'med-magazin': get_product_med_magazin,
                 # 'iherbgroup': get_product_iherbgroup,
 
