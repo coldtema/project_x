6ffb4d5b52b1f07969f202014521bd3a68869117
coldtema <coldtema1703@gmail.com>
2025-03-27 01:52:54 +0300

    added func check_existence_of_brand_and_seller

--- apps/wb_checker/backend_explorer.py
+++ apps/wb_checker/backend_explorer.py
@@ -2,7 +2,7 @@ import re
 from .models import WBProduct, WBBrand, WBSeller
 from apps.blog.models import Author
 from django.http import HttpResponse
-from wb_explorer import get_product_info
+import apps.wb_checker.wb_explorer as wb_explorer 
 
 def check_repetitions_product(product_url, author_id):
     artikul = re.search(r'\/(\d+)\/', product_url).group(1)
@@ -17,8 +17,22 @@ def check_repetitions_product(product_url, author_id):
                 return
         repeated_product.authors.add(Author.objects.get(id=author_id)) #если не нашел автора и не выкинуло из функции, то добавляет many-to-many связь
     else:
-        get_product_info(product_url, author_id)
+        wb_explorer.get_product_info(product_url, author_id)
 
+#проверяю на наличие бренда и продавца в БД
+def check_existence_of_brand_and_seller(seller_dict, brand_dict):
+    brand_existence = WBBrand.objects.filter(wb_id=brand_dict['brand_id'])
+    seller_existence = WBSeller.objects.filter(wb_id=seller_dict['seller_id'])
+    if not brand_existence:
+        WBBrand.objects.create(name=brand_dict['brand_name'],
+                               wb_id=brand_dict['brand_id'],
+                               main_url=f'https://www.wildberries.ru/brands/{brand_dict['brand_id']}',
+                               full_control = False)
+    if not seller_existence:
+        WBSeller.objects.create(name=seller_dict['seller_name'],
+                               wb_id=seller_dict['seller_id'],
+                               main_url=f'https://www.wildberries.ru/seller/{seller_dict['seller_id']}',
+                               full_control = False)
 
 def check_repetitions_seller(product_url, author_id):
     ...
