52df47c11a75650c83e1eca799d90ab42f21dbf5
coldtema <coldtema1703@gmail.com>
2025-03-27 00:12:25 +0300

    changes in main view_func

--- apps/wb_checker/views.py
+++ apps/wb_checker/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render
 from django.urls import reverse
 from .forms import WBProductForm
 from django.http import HttpResponseRedirect, HttpResponse
+import apps.wb_checker.backend_explorer as backend_explorer
 
 
+
+action_type_dict = {'product': backend_explorer.check_repetitions_product, 
+             'brand': backend_explorer.check_repetitions_brand, 
+             'seller': backend_explorer.check_repetitions_seller,
+}
 def all_price_list(request):
     form = WBProductForm()
     if request.method == 'POST':
         form = WBProductForm(request.POST)
         if form.is_valid():
-            #проверяем поле action_type, чтобы понять какую функцию юзать
-
-            #если просто продукт:
-            #проверка на дубликат (полная база)
-            #если дубликат, то у продукта, который продублировался меняем поле authors (просто добавляем новую many-to-many связь)
+            author_id = 2 #пока не знаю, как точно передавать author_id в функцию, но это как-то через аунтефикацию надо делать (пока эмулирую)
+            #проверяем поле action_type, чтобы понять какую функцию юзать (можно как альтернативу сделать regex's по урлу, но пока проще так)
+            action_type=request.POST['action_type']
+            action_type_dict[action_type](request.POST['url'], author_id)
 
             #если бренд, то прежде всего проверяем, сколько всего товаров в total на api (если total > 1000, то пока выдаем ошибку (но будем выдавать варианты (все, первая 100, кастомное значение)))
             #если все ок, то:
