86f1451ae6cdf909b45328189668da5d67b625c7
coldtema <coldtema1703@gmail.com>
2025-03-26 20:17:18 +0300

    added WBProduct model and migrated it

--- apps/price_checker/models.py
+++ apps/price_checker/models.py
@@ -118,3 +118,40 @@ class WBBrand(models.Model):
     def __str__(self):
         return self.name
     
+
+
+class WBProduct(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя продукта WB')
+    artikul = models.CharField(max_length=100, verbose_name='Артикул продукта WB')
+    latest_price = models.IntegerField(verbose_name='Последняя цена')
+    wb_cosh = models.BooleanField(default=True)
+    # shop_name = models.ForeignKey(max_length=100, verbose_name='Продавец продукта WB') - будет в другой таблице
+    seller = models.ForeignKey(WBSeller, on_delete=models.CASCADE, verbose_name='Продавец продукта WB')
+    # brand_name = models.CharField(max_length=100, verbose_name='Бренд продукта WB') - будет в другой таблице
+    brand = models.ForeignKey(WBBrand, on_delete = models.CASCADE, verbose_name='Бренд продукта WB')
+    url = models.URLField(verbose_name='URL')
+    enabled = models.BooleanField(default=True)
+    authors = models.ManyToManyField(Author, verbose_name='Никнеймы авторов')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления продукта WB')
+    updated = models.DateTimeField(default=timezone.now, verbose_name='Время обновления продукта WB')
+    objects = models.Manager()
+    enabled_products = EnabledManager()
+    class Meta:
+        verbose_name = 'Продукт WB'
+        verbose_name_plural = 'Продукты WB'
+        ordering = ['-updated']
+        # indexes = [
+        #     models.Index(fields=['authors']),
+        #     ]
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if self.pk:  # Проверяем, что объект уже существует (не новый)
+            original = Product.objects.get(pk=self.pk)
+            if self.latest_price != original.latest_price:  # Проверяем, изменилось ли нужное поле
+                self.updated = timezone.now()  # Обновляем вручную
+            else:
+                self.updated = original.updated
+        super().save(*args, **kwargs)
