9665b19c281c5067696730c582e77e97e1b3b45e
coldtema <coldtema1703@gmail.com>
2025-03-29 16:27:10 +0300

    added path of clearing DB in urls

--- apps/wb_checker/urls.py
+++ apps/wb_checker/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from apps.wb_checker import views
 
 urlpatterns = [
-    path('', views.all_price_list, name='all_price_list')
+    path('', views.all_price_list, name='all_price_list'),
+    path('clear_db/', views.clear_db, name='clear_db')
 ]
