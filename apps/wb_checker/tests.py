b37e5cbefb837796a85dfaad2679c3564df5f7b5
coldtema <coldtema1703@gmail.com>
2025-03-31 04:10:52 +0300

    added tests)))))))))))))

--- apps/wb_checker/tests.py
+++ apps/wb_checker/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
-# Create your tests here.
+#URLS FOR BRANDS
+# https://www.wildberries.ru/brands/joonies
+# https://www.wildberries.ru/brands/311409788-cotton-care
+# https://www.wildberries.ru/brands/modjimoda/pizhamy
+# https://www.wildberries.ru/brands/erichkrause/all?xsubject=745;1369&fsupplier=32693&bid=def0e247-7c66-4964-9eb5-ad3635244dc7
+# https://www.wildberries.ru/brands/erichkrause/all?sort=popular&page=1&priceU=12500%3B119000&xsubject=719%3B3920%3B3181%3B8334%3B1866%3B661&fsupplier=32693
+# https://www.wildberries.ru/brands/erichkrause/kalkulyatory?sort=popular
+
+
+
+#URLS FOR SELLERS
+# https://www.wildberries.ru/seller/simaland-exclusive
+# https://www.wildberries.ru/seller/simaland-exclusive?page=1&sort=priceup&fbrand=28172
+# https://www.wildberries.ru/seller/kari/botinki?page=1&sort=priceup&fbrand=16819%3B4254%3B16824%3B16818%3B15724&frating=1&foriginal=1&ffeedbackpoints=1&f1000000003=-1000000059%3B-1000000061%3B-1000000063%3B-1000000065%3B-1000000067&fcolor=16119260%3B0&f14177450=15000583%3B15000921%3B15001872&f57459=64695%3B90233&f4=9844%3B9780
+# https://www.wildberries.ru/seller/688746/botilony?page=1&sort=priceup
+# https://www.wildberries.ru/seller/125043/uhod-za-ukrasheniyami?page=1&sort=benefit&priceU=34600%3B54600&foriginal=1&f1676801=4577834%3B3919359
+# https://www.wildberries.ru/brands/lana/sprei?page=1&sort=benefit&fdlvr=24
+# https://www.wildberries.ru/seller/e-cosmetic
+# https://www.wildberries.ru/seller/90633/bb-kremy?page=1&sort=benefit&fbrand=12684%3B12683%3B15149%3B8693%3B24519%3B22701%3B90731&frating=1&foriginal=1&f14177450=26378252%3B15000554%3B15000418%3B15001772%3B15000865%3B15000721%3B15001001%3B15001253%3B26182531%3B15000673%3B15000843%3B15001702&f56267=63524%3B63529&f10877=32368%3B4673081&f89010=-14015%3B-14017
+# https://www.wildberries.ru/seller/simaland-exclusive?sort=popular&page=1&priceU=5400%3B27700#c260395993
+
+
+
+#URLS FOR PRODUCTS
+# https://www.wildberries.ru/catalog/200579237/detail.aspx
+# https://www.wildberries.ru/catalog/78122415/detail.aspx
+# https://www.wildberries.ru/catalog/216972285/detail.aspx
+# https://www.wildberries.ru/catalog/241952387/detail.aspx
+# https://www.wildberries.ru/catalog/250909718/detail.aspx
