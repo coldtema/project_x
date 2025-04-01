35aac8c77a8bea6f8df502b0268a57ef59273e6f
coldtema <coldtema1703@gmail.com>
2025-04-01 22:22:51 +0300

    added funcs for updating and saving prices in DB in utils.py

--- apps/wb_checker/utils.py
+++ apps/wb_checker/utils.py
@@ -1,8 +1,12 @@
+import math
 import time
 from functools import wraps
-from .models import WBBrand, WBSeller
+from .models import WBBrand, WBSeller, WBProduct, WBPrice
 import cloudscraper
 import json
+from django.utils import timezone
+from django.db import transaction
+
 
 def time_count(func):
     @wraps(func)
@@ -61,3 +65,47 @@ def update_categories():
                 wrapper(elem['childs'])
         return categories_list
     return wrapper(json_data)
+
+@transaction.atomic
+def save_update_prices(updated_prods, updated_prices):
+    WBProduct.objects.bulk_update(updated_prods, ['latest_price'])
+    WBPrice.objects.bulk_create(updated_prices)
+
+
+
+def update_prices():
+    product_url_api = f'https://card.wb.ru/cards/v2/list?appType=1&curr=rub&dest=-1257786&spp=30&ab_testing=false&lang=ru&nm='
+    #получает все продукиы в виде артикул:продукт
+    all_prods = list(WBProduct.enabled_products.all())
+    all_prods_artikuls = list(map(lambda x: x.artikul, WBProduct.enabled_products.all()))
+    updated_prods = []
+    updated_prices = []
+    #максимум в листе 512 элементов
+    for i in range(math.ceil(len(all_prods) / 512)):
+        temp_prods_from_db = list(map(lambda x: x.artikul, all_prods[512*i:512*(i+1)]))
+        final_url = product_url_api + ';'.join(temp_prods_from_db)
+        headers = {"User-Agent": "Mozilla/5.0"}
+        scraper = cloudscraper.create_scraper()
+        response = scraper.get(final_url, headers=headers)
+        json_data = json.loads(response.text)
+        products_on_page = json_data['data']['products']
+        for j in range(len(products_on_page)):
+            product_artikul = products_on_page[j]['id']
+            product_price = products_on_page[j]['sizes'][0]['price']['product'] // 100
+            if str(product_artikul) in all_prods_artikuls:
+                product_to_check = all_prods[all_prods_artikuls.index(str(product_artikul))]
+                if product_to_check.latest_price != product_price: #проверить на всякий случай на типы здесь
+                    product_to_check.latest_price = product_price
+                    updated_prods.append(product_to_check)
+                    updated_prices.append(WBPrice(price=product_price,
+                            added_time=timezone.now(),
+                            product=product_to_check))
+        #сверяем количество полученных продуктов и переданных продуктов
+        if len(products_on_page) != len(temp_prods_from_db):
+            # check_disabled_prods()
+            ...
+    save_update_prices(updated_prods, updated_prices)
+
+
+
+
