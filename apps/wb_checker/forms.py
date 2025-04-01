1bad8a392f4a922f001f0873fd9eaef701305f57
coldtema <coldtema1703@gmail.com>
2025-04-01 03:48:27 +0300

    added choice for promo link

--- apps/wb_checker/forms.py
+++ apps/wb_checker/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 ACTION_TYPE=(('product', 'product'), 
              ('brand', 'brand'), 
-             ('seller', 'seller'))
+             ('seller', 'seller'),
+             ('promo', 'promo'))
 
 class WBProductForm(forms.Form):
     url = forms.CharField(max_length=1000, required=True)
