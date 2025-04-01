4f4cd69c1d89af3390b223d7438c928627ccf34f
coldtema <coldtema1703@gmail.com>
2025-04-01 22:30:33 +0300

    added file with funcs related to pickpoints (they get dest of wb using text-address of pickpoint)

---
+++ apps/wb_checker/wb_pickpoints.py
@@ -0,0 +1,29 @@
+import requests
+import os
+import cloudscraper
+
+
+def get_coordinates(address):
+    url = f"https://geocode-maps.yandex.ru/v1/?"
+    params = {
+        "apikey": os.getenv('YANDEX_MAPS_API_KEY'),
+        "geocode": address,
+        "format": 'json'
+    }
+
+    response = requests.get(url, params=params)
+    json_data = response.json()
+    coordinates = json_data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split()
+    latitude = coordinates[1]
+    longitude = coordinates[0]
+    return latitude, longitude
+
+
+def get_dest(address):
+    latitude, longitude = get_coordinates(address)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    url = f'https://user-geo-data.wildberries.ru/get-geo-info?currency=RUB&latitude={latitude}&longitude={longitude}&locale=ru&dt=1743520473&currentLocale=ru&b2bMode=false&addressType=self'
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(url, headers=headers)
+    json_data = response.json()
+    return json_data['destinations'][-1], address
