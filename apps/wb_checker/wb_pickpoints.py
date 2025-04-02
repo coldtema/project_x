ad2a0b05c33a5389d3b1b68c02a3a44c33c74d86
coldtema <coldtema1703@gmail.com>
2025-04-02 22:20:43 +0300

    added docstrings

--- apps/wb_checker/wb_pickpoints.py
+++ apps/wb_checker/wb_pickpoints.py
@@ -1,17 +1,17 @@
-import requests
 import os
 import cloudscraper
 
 
 def get_coordinates(address):
+    '''Берет широту и долготу через api яндекс карт'''
     url = f"https://geocode-maps.yandex.ru/v1/?"
     params = {
         "apikey": os.getenv('YANDEX_MAPS_API_KEY'),
         "geocode": address,
         "format": 'json'
     }
-
-    response = requests.get(url, params=params)
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(url, params=params)
     json_data = response.json()
     coordinates = json_data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split()
     latitude = coordinates[1]
@@ -20,6 +20,7 @@ def get_coordinates(address):
 
 
 def get_dest(address):
+    '''Получает id широты и долготы на wb'''
     latitude, longitude = get_coordinates(address)
     headers = {"User-Agent": "Mozilla/5.0"}
     url = f'https://user-geo-data.wildberries.ru/get-geo-info?currency=RUB&latitude={latitude}&longitude={longitude}&locale=ru&dt=1743520473&currentLocale=ru&b2bMode=false&addressType=self'
