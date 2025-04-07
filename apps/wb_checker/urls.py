f7c29a06936802a2eb6bdf20374c9126594fb68d
coldtema <coldtema1703@gmail.com>
2025-04-08 01:14:45 +0300

    added path of updating avaliability

--- apps/wb_checker/urls.py
+++ apps/wb_checker/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.all_price_list, name='all_price_list'),
     path('clear_db/', views.clear_db, name='clear_db'),
     path('update_categories/', views.update_brands_categories, name='update_categories'),
-    path('update_prices/', views.update_prices, name='update_prices')
+    path('update_prices/', views.update_prices, name='update_prices'),
+    path('update_avaliability/', views.update_avaliability, name='update_avaliability')
 ]
