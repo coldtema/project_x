a28767054b15d858dabc916fb9062d83e1bbda1d
coldtema <coldtema1703@gmail.com>
2025-04-10 01:08:03 +0300

    added url for loading test data

--- apps/wb_checker/urls.py
+++ apps/wb_checker/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('clear_db/', views.clear_db, name='clear_db'),
     path('update_categories/', views.update_brands_categories, name='update_categories'),
     path('update_prices/', views.update_prices, name='update_prices'),
-    path('update_avaliability/', views.update_avaliability, name='update_avaliability')
+    path('update_avaliability/', views.update_avaliability, name='update_avaliability'),
+    path('load_test_data', views.load_test_data, name='load_test_data')
 ]
