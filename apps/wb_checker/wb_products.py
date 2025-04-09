bcf1d21e47d8c3f6691a81ab952ee41e90ca197d
coldtema <coldtema1703@gmail.com>
2025-04-09 03:52:18 +0300

    totally rewrited class for parsing 1 product

--- apps/wb_checker/wb_products.py
+++ apps/wb_checker/wb_products.py
@@ -7,7 +7,7 @@ import json
 import cloudscraper
 from datetime import datetime
 import apps.wb_checker.utils as utils
-from .models import WBProduct, WBSeller, WBBrand, WBPrice
+from .models import WBProduct, WBSeller, WBBrand, WBPrice, WBDetailedInfo
 from apps.blog.models import Author
 from django.utils import timezone
 import math
@@ -26,85 +26,38 @@ class Product:
         self.scraper = cloudscraper.create_scraper()
         self.artikul = re.search(r'\/(\d+)\/', product_url).group(1)
         self.product_url_api = f'https://card.wb.ru/cards/v2/detail?appType=1&curr=rub&dest={self.author_object.dest_id}&spp=30&ab_testing=false&lang=ru&nm={self.artikul}'
-        self.product_size = self.get_product_size()
+        self.product_name, self.product_size, self.product_volume, self.product_price = self.get_product_detailed_info()
 
-
-    def get_product_size(self):
-        response = self.scraper.get(self.product_url_api, headers=self.headers)
-        json_data = json.loads(response.text)
-        sizes = json_data['data']['products'][0]['sizes']
-        sizes_dict = dict()
-        for size in sizes:
-            volume_of_size = 0
-            for stock in size['stocks']:
-                volume_of_size += stock['qty']
-            sizes_dict.update({size['origName']: volume_of_size})
-        print(f'Товар обнаружен! Доступные варианты:')
-        for size, volume in sizes_dict.items():
-            print(f'Вариант: {size}. Количество: {volume}')
-        print('Введите имя варианта, который нужно выбрать')
-        while True:
-            user_size = input()
-            if sizes_dict.get(user_size) is None: 
-                print('Введено неправильное имя варианта, попробуйте снова')
-                continue
-            break
-        if sizes_dict.get(user_size) == 0:
-            print('Вы выбрали товар, которого нет в наличии. Он добавлен в список отслеживаемого')
-        else:
-            print('Товар добавлен в отслеживаемые')
-        
-    @time_count
-    def get_repetition_or_run(self):
-        '''Проверка на повторки среди считываемых продуктов и продуктов бренда, которые уже существуют в БД'''
-        repeated_product = WBProduct.objects.filter(artikul=self.artikul) #посмотреть, как сделать так, чтобы get - функция не выдавала исключения
-        if repeated_product and len(repeated_product) == 1: #если нашел повторюшку
-            repeated_product = repeated_product[0]
-            authors_list = repeated_product.enabled_connection.all() #проверяет, нет ли уже того же автора у этой повторюшки
-            for elem in authors_list: #выводит ворнинг, если такой же автор
-                if elem.id == self.author_id:
-                    print('Товар уже есть в отслеживании')
-                    return
-                if elem.dest_id == self.author_object.dest_id:
-                    repeated_product.enabled_connection.add(Author.objects.get(id=self.author_id)) #если не нашел автора и не выкинуло из функции, то добавляет many-to-many связь (попробовать написать через автора)
-                    return
-        self.get_product_info()
-
-
+    @utils.time_count
     def get_product_info(self):
-        '''Функция сборки нового продукта (если не обнаружен дубликат)'''
-        #полностью собирает элемент
+        '''Функция сборки продукта'''
+        if self.product_volume == 0: enabled = False 
+        else: enabled = True
         response = self.scraper.get(self.product_url_api, headers=self.headers)
         json_data = json.loads(response.text)
-        try:
-            name = json_data['data']['products'][0]['name']
-        except:
-            print('Похоже, что товар не доступен в вашем регионе')
-            return
-        price_element = json_data['data']['products'][0]['sizes'][0]['price']['product'] // 100
-        try: #если вдруг истории цены нет
-            price_history = self.get_price_history()
-            price_history = list(map(lambda x: (timezone.make_aware(datetime.fromtimestamp(x['dt'])), x['price']['RUB']//100), price_history))
-            price_history.append((timezone.now(), price_element))
-        except:
-            price_history = [(timezone.now(), price_element)]
         seller_name = json_data['data']['products'][0]['supplier'] #имя продавца
         seller_artikul = json_data['data']['products'][0]['supplierId'] #id продавца
         brand_name = json_data['data']['products'][0]['brand'] #имя бренда
         brand_artikul = json_data['data']['products'][0]['brandId'] #id бренда
-        #проверяет наличие этого бренда и продавца в БД (если нет, то создает их, если есть - не трогает)
         brand_object = Product.build_raw_brand_object(brand_name, brand_artikul)
         seller_object = Product.build_raw_seller_object(seller_name, seller_artikul)  
         #добавляем элемент
-        new_product = WBProduct(name=name,
+        new_product = WBProduct(name=self.product_name,
                 artikul=self.artikul,
-                latest_price=price_element,
                 wb_cosh=True,
-                url=self.product_url,
+                url=self.product_url.split('?')[0],
                 seller=seller_object,
                 brand=brand_object)
-        price_history = list(map(lambda x: WBPrice(price=x[1], added_time=x[0], product=new_product), price_history))
-        self.add_product_to_db(new_product, price_history)
+        new_detailed_info = WBDetailedInfo(latest_price=self.product_price,
+                                           size=self.product_size,
+                                           volume=self.product_volume,
+                                           enabled=enabled,
+                                           author_id=self.author_id,
+                                           product=new_product)
+        new_price = WBPrice(price=self.product_price,
+                            added_time=timezone.now(),
+                            detailed_info=new_detailed_info)
+        self.add_product_to_db(new_product, new_detailed_info, new_price)
 
 
     @staticmethod
@@ -124,99 +77,139 @@ class Product:
     
 
 
-    def get_price_history(self):
-        '''Функция получения истории цены продукта'''
-        basket_num = Product.get_basket_num(int(self.artikul))
-        if basket_num < 10:
-            basket_num = f'0{basket_num}'
-        if len(self.artikul) == 9:
-            price_history_searcher_url = f'https://basket-{basket_num}.wbbasket.ru/vol{self.artikul[:4]}/part{self.artikul[:6]}/{self.artikul}/info/price-history.json'
-        elif len(self.artikul) == 9:
-            price_history_searcher_url = f'https://basket-{basket_num}.wbbasket.ru/vol{self.artikul[:3]}/part{self.artikul[:5]}/{self.artikul}/info/price-history.json'
-        elif len(self.artikul) == 7:
-            price_history_searcher_url = f'https://basket-{basket_num}.wbbasket.ru/vol{self.artikul[:2]}/part{self.artikul[:4]}/{self.artikul}/info/price-history.json'
-        elif len(self.artikul) == 6:
-            price_history_searcher_url = f'https://basket-{basket_num}.wbbasket.ru/vol{self.artikul[:1]}/part{self.artikul[:3]}/{self.artikul}/info/price-history.json'
-        response = self.scraper.get(price_history_searcher_url, headers=self.headers)
+    def get_product_detailed_info(self):
+        response = self.scraper.get(self.product_url_api, headers=self.headers)
         json_data = json.loads(response.text)
-        return json_data
+        name = json_data['data']['products'][0]['name']
+        sizes = json_data['data']['products'][0]['sizes']
+        sizes_dict = dict()
+        for size in sizes:
+            volume_of_size = 0
+            price_of_size = 0
+            if len(size['stocks']) != 0:
+                price_of_size = size['price']['product']//100
+            for stock in size['stocks']:
+                volume_of_size += stock['qty']
+            sizes_dict.update({size['origName']: (volume_of_size, price_of_size)})
+        if len(list(sizes_dict.keys())) == 1 and list(sizes_dict.keys())[0] == '0':
+            print(f'Товар обнаружен!\nНазвание: {name}.\nПрисутствует только 1 вариант.')
+            return name, None, sizes_dict['0'][0], sizes_dict['0'][1]
+        print(f'Товар обнаружен!\nНазвание: {name}.\nДоступные варианты:')
+        for size, volume_and_price in sizes_dict.items():
+            print(f'Вариант: {size}. Количество: {volume_and_price[0]}. Цена: {volume_and_price[1]}')
+        print('Введите имя варианта, который нужно выбрать')
+        while True:
+            user_size = input()
+            if sizes_dict.get(user_size) is None: 
+                print('Введено неправильное имя варианта, попробуйте снова')
+                continue
+            break
+        return name, user_size, sizes_dict[user_size][0], sizes_dict[user_size][1]
     
-
+    @utils.time_count
     @transaction.atomic
-    def add_product_to_db(self, new_product, price_history):
+    def add_product_to_db(self, new_product, new_detailed_info, new_price):
         '''Функция добавления всех изменений в БД атомарной транзакцией'''
         #сохраняем элемент
         WBBrand.objects.bulk_create([new_product.brand], update_conflicts=True, unique_fields=['wb_id'], update_fields=['name'])
         WBSeller.objects.bulk_create([new_product.seller], update_conflicts=True, unique_fields=['wb_id'], update_fields=['name'])
-        already_added_product = WBProduct.objects.update_or_create(artikul=new_product.artikul, defaults={'name':new_product.name,
-                                                                                           'latest_price':new_product.latest_price,
-                                                                                           'wb_cosh':True,
-                                                                                           'url':self.product_url,
-                                                                                           'seller':new_product.seller,
-                                                                                           'brand':new_product.brand})
-        if len(already_added_product[0].wbprice_set.all()) <= 1: #если добавилась при бренде отслеживании от другого процесса только одна цена (а у меня в price_history лежит история для одиночки)
-            already_added_product[0].wbprice_set.all().delete()
-            for elem in price_history:  
-                elem.product_id = already_added_product[0].id
-            WBPrice.objects.bulk_create(price_history)
-        Author.objects.get(id=self.author_id).enabled_connection.add(already_added_product[0])
+        WBProduct.objects.bulk_create([new_product], update_conflicts=True, unique_fields=['artikul'], update_fields=['name'])
+        new_detailed_info.save()
+        new_price.save()
+        self.author_object.slots -= 1
+        self.author_object.save()
     
 
 
-    @staticmethod
-    def get_basket_num(artikul: int):
-        '''Определение сервера, на котором находится история цены по js скрипту на wb'''
-        s = artikul // 100000  # Разделение артикулов на группы
-        if s <= 143:
-            return 1
-        elif s <= 287:
-            return 2
-        elif s <= 431:
-            return 3
-        elif s <= 719:
-            return 4
-        elif s <= 1007:
-            return 5
-        elif s <= 1061:
-            return 6
-        elif s <= 1115:
-            return 7
-        elif s <= 1169:
-            return 8
-        elif s <= 1313:
-            return 9
-        elif s <= 1601:
-            return 10
-        elif s <= 1655:
-            return 11
-        elif s <= 1919:
-            return 12
-        elif s <= 2045:
-            return 13
-        elif s <= 2189:
-            return 14
-        elif s <= 2405:
-            return 15
-        elif s <= 2621:
-            return 16
-        elif s <= 2837:
-            return 17
-        elif s <= 3053:
-            return 18
-        elif s <= 3269:
-            return 19
-        elif s <= 3485:
-            return 20
-        elif s <= 3701:
-            return 21
-        elif s <= 3917:
-            return 22
-        elif s <= 4133:
-            return 23
-        elif s <= 4349:
-            return 24
-        elif s <= 4565:
-            return 25
-        else:
-            return 26
+    # @staticmethod
+    # def get_basket_num(artikul: int):
+    #     '''Определение сервера, на котором находится история цены по js скрипту на wb'''
+    #     s = artikul // 100000  # Разделение артикулов на группы
+    #     if s <= 143:
+    #         return 1
+    #     elif s <= 287:
+    #         return 2
+    #     elif s <= 431:
+    #         return 3
+    #     elif s <= 719:
+    #         return 4
+    #     elif s <= 1007:
+    #         return 5
+    #     elif s <= 1061:
+    #         return 6
+    #     elif s <= 1115:
+    #         return 7
+    #     elif s <= 1169:
+    #         return 8
+    #     elif s <= 1313:
+    #         return 9
+    #     elif s <= 1601:
+    #         return 10
+    #     elif s <= 1655:
+    #         return 11
+    #     elif s <= 1919:
+    #         return 12
+    #     elif s <= 2045:
+    #         return 13
+    #     elif s <= 2189:
+    #         return 14
+    #     elif s <= 2405:
+    #         return 15
+    #     elif s <= 2621:
+    #         return 16
+    #     elif s <= 2837:
+    #         return 17
+    #     elif s <= 3053:
+    #         return 18
+    #     elif s <= 3269:
+    #         return 19
+    #     elif s <= 3485:
+    #         return 20
+    #     elif s <= 3701:
+    #         return 21
+    #     elif s <= 3917:
+    #         return 22
+    #     elif s <= 4133:
+    #         return 23
+    #     elif s <= 4349:
+    #         return 24
+    #     elif s <= 4565:
+    #         return 25
+    #     else:
+    #         return 26
 
+
+
+    # @time_count
+    # def get_repetition_or_run(self):
+    #     '''Проверка на повторки среди считываемых продуктов и продуктов бренда, которые уже существуют в БД'''
+
+    #     repeated_product = WBProduct.objects.filter(artikul=self.artikul) #посмотреть, как сделать так, чтобы get - функция не выдавала исключения
+    #     if repeated_product and len(repeated_product) == 1: #если нашел повторюшку
+    #         repeated_product = repeated_product[0]
+    #         authors_list = repeated_product.enabled_connection.all() #проверяет, нет ли уже того же автора у этой повторюшки
+    #         for elem in authors_list: #выводит ворнинг, если такой же автор
+    #             if elem.id == self.author_id:
+    #                 print('Товар уже есть в отслеживании')
+    #                 return
+    #             if elem.dest_id == self.author_object.dest_id:
+    #                 repeated_product.enabled_connection.add(Author.objects.get(id=self.author_id)) #если не нашел автора и не выкинуло из функции, то добавляет many-to-many связь (попробовать написать через автора)
+    #                 return
+    #     self.get_product_info()
+
+    # def get_price_history(self):
+    #     '''Функция получения истории цены продукта'''
+    #     basket_num = Product.get_basket_num(int(self.artikul))
+    #     if basket_num < 10:
+    #         basket_num = f'0{basket_num}'
+    #     if len(self.artikul) == 9:
+    #         price_history_searcher_url = f'https://basket-{basket_num}.wbbasket.ru/vol{self.artikul[:4]}/part{self.artikul[:6]}/{self.artikul}/info/price-history.json'
+    #     elif len(self.artikul) == 9:
+    #         price_history_searcher_url = f'https://basket-{basket_num}.wbbasket.ru/vol{self.artikul[:3]}/part{self.artikul[:5]}/{self.artikul}/info/price-history.json'
+    #     elif len(self.artikul) == 7:
+    #         price_history_searcher_url = f'https://basket-{basket_num}.wbbasket.ru/vol{self.artikul[:2]}/part{self.artikul[:4]}/{self.artikul}/info/price-history.json'
+    #     elif len(self.artikul) == 6:
+    #         price_history_searcher_url = f'https://basket-{basket_num}.wbbasket.ru/vol{self.artikul[:1]}/part{self.artikul[:3]}/{self.artikul}/info/price-history.json'
+    #     response = self.scraper.get(price_history_searcher_url, headers=self.headers)
+    #     json_data = json.loads(response.text)
+    #     return json_data
