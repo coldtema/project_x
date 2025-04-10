6e37bb396504d115d60b5dea84af6bd7b3361dc2
coldtema <coldtema1703@gmail.com>
2025-04-10 20:27:38 +0300

    divided 2 big funcs in 3 small + added new attribute - current detail to check in class AvaliabilityUpdater

--- apps/wb_checker/utils.py
+++ apps/wb_checker/utils.py
@@ -58,6 +58,7 @@ class PriceUpdater:
         self.new_prices = []
         self.updated_details = []
         self.test_counter = 0
+        self.current_detail_to_check = None
 
 
     def run(self):
@@ -176,6 +177,7 @@ class AvaliabilityUpdater:
         self.new_prices = []
         self.updated_details = []
         self.test_counter = 0
+        self.current_detail_to_check = None
 
 
     def run(self):
@@ -203,18 +205,18 @@ class AvaliabilityUpdater:
             details_of_prods_to_check = sorted(details_of_prods_to_check, key=lambda x: x.product.artikul)
 
             for j in range(len(products_on_page)):
-                current_detail_to_check = details_of_prods_to_check[j]
-                if products_on_page[j]['id'] == current_detail_to_check.product.artikul: #по хорошему вот тут надо добавить исключение какое то
-                    if current_detail_to_check.size == None:
-                        self.check_nonsize_product(current_detail_to_check, products_on_page[j])
+                self.current_detail_to_check = details_of_prods_to_check[j]
+                if products_on_page[j]['id'] == self.current_detail_to_check.product.artikul: #по хорошему вот тут надо добавить исключение какое то
+                    if self.current_detail_to_check.size == None:
+                        self.check_nonsize_product(products_on_page[j])
                     else:
-                        self.check_size_product(current_detail_to_check, products_on_page[j])                      
+                        self.check_size_product(products_on_page[j])                      
                 else:
                     print('Не сходится товар и запрос по индексам')
 
 
 
-    def check_nonsize_product(self, current_detail_to_check, product_on_page):
+    def check_nonsize_product(self, product_on_page):
         '''Функция проверки наличия продукта, у которого нет размеров'''
         self.test_counter += 1
         stocks = product_on_page['sizes'][0]['stocks']
@@ -225,24 +227,17 @@ class AvaliabilityUpdater:
                 volume += stock['qty']
             price_of_detail = product_on_page['sizes'][0]['price']['product'] // 100
             #проверять тут нужно на разницу цены до "нет в наличии" и после
-            current_detail_to_check.latest_price = price_of_detail
-            current_detail_to_check.volume = volume
-            current_detail_to_check.enabled = True
-            self.updated_details.append(current_detail_to_check)
-            self.new_prices.append(WBPrice(price=price_of_detail,
-                                            added_time=timezone.now(),
-                                            detailed_info=current_detail_to_check))
-            print(f'Продукт снова в наличии!\nПродукт: {current_detail_to_check.product.url}\n')
+            self.enable_product(price_of_detail, volume)
             
 
 
 
-    def check_size_product(self, current_detail_to_check, product_on_page):
+    def check_size_product(self, product_on_page):
         '''Функция проверки наличия продукта, у которого есть размер'''
         self.test_counter += 1
         sizes = product_on_page['sizes']
         for size in sizes:
-            if size['origName'] == current_detail_to_check.size:
+            if size['origName'] == self.current_detail_to_check.size:
                 stocks = size['stocks']
                 if len(stocks) != 0:
                     #точка входа для уведомления пользователя о том, что товар в наличии
@@ -250,16 +245,21 @@ class AvaliabilityUpdater:
                     for stock in stocks:
                         volume += stock['qty']
                     price_of_detail = size['price']['product'] // 100
-                    current_detail_to_check.latest_price = price_of_detail
-                    current_detail_to_check.volume = volume
-                    current_detail_to_check.enabled = True
-                    self.updated_details.append(current_detail_to_check)
-                    self.new_prices.append(WBPrice(price=price_of_detail,
-                                                    added_time=timezone.now(),
-                                                    detailed_info=current_detail_to_check))
-                    print(f'Продукт снова в наличии!\nПродукт: {current_detail_to_check.product.url}\n') #добавить кастомизацию по размеру (вдруг его не было в наличии)
+                    self.enable_product(price_of_detail, volume) #добавить кастомизацию по размеру (вдруг его не было в наличии)
                 break                      
                 
+
+
+    def enable_product(self, price_of_detail, volume):
+        self.current_detail_to_check.latest_price = price_of_detail
+        self.current_detail_to_check.volume = volume
+        self.current_detail_to_check.enabled = True
+        self.updated_details.append(self.current_detail_to_check)
+        self.new_prices.append(WBPrice(price=price_of_detail,
+                                        added_time=timezone.now(),
+                                        detailed_info=self.current_detail_to_check))
+        print(f'Продукт снова в наличии!\nПродукт: {self.current_detail_to_check.product.url}\n')
+
 
 
     @time_count
