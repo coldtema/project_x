f0dc4cb76604cd032365e1f8567743c78536fad7
coldtema <coldtema1703@gmail.com>
2025-04-11 04:28:56 +0300

    changed name of url for correct reverse

--- apps/price_checker/urls.py
+++ apps/price_checker/urls.py
@@ -4,7 +4,7 @@ from apps.price_checker import views
 
 
 urlpatterns = [
-    path('', views.all_price_list, name='all_price_list'),
+    path('', views.all_price_list, name='all_price_list_1'),
     path('update_prices/', views.update_prices, name='update_prices'),
     path('price_history/<int:id>', views.price_history, name='price_history'),
     path('delete_product/<int:id>', views.delete_product, name='delete_product'),
