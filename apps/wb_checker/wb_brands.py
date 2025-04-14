4161758e62c4ca5720ad43773374dc812f9a6dbc
coldtema <coldtema1703@gmail.com>
2025-04-14 23:58:20 +0300

    rewrited class brand in wb_checker

--- apps/wb_checker/wb_brands.py
+++ apps/wb_checker/wb_brands.py
@@ -6,13 +6,14 @@ import json
 import cloudscraper
 from datetime import datetime
 import apps.wb_checker.utils as utils
-from .models import WBProduct, WBSeller, WBPrice, WBCategory, WBBrand, WBPreset
+from .models import WBProduct, WBSeller, WBPrice, WBCategory, WBBrand, WBPreset, TopWBProduct
 from apps.blog.models import Author
 from django.utils import timezone
 import math
 import time
 from functools import wraps
 from django.db import transaction
+from statistics import median
 
 
 
@@ -30,14 +31,10 @@ class Brand:
         self.brand_api_url = self.construct_brand_api_url()
         self.total_products, self.brand_name = self.get_total_products_and_name_brand_in_catalog()
         if self.dest_avaliable:
-            self.number_of_pages = self.get_number_of_pages_in_catalog()
             self.brand_object = self.build_raw_brand_object()
-            self.preset_object = self.build_raw_preset_object(raw_brand_url)
-            self.potential_repetitions = self.get_repetitions_catalog_brand()
-            self.sellers_in_db_dict = dict(map(lambda x: (x.wb_id, x), WBSeller.objects.all()))
-            self.product_repetitions_list = []
-            self.sellers_to_add = []
-            self.brand_products_to_add = []
+            self.dict_brand_products_to_add = dict()
+            self.dict_sellers_to_add = dict()
+            self.list_brand_products_to_add_with_scores = []
 
 
 
@@ -45,39 +42,47 @@ class Brand:
     def run(self):
         '''Запуск процесса парсинга'''
         if self.dest_avaliable:
-            self.get_catalog_of_brand()
+            self.get_catalog_of_brand('popular')
+            self.get_catalog_of_brand('benefit')
+            self.get_catalog_of_brand('rate')
+            self.build_top_prods()
             self.add_all_to_db()
     
 
 
     @utils.time_count
-    def get_catalog_of_brand(self):
+    def get_catalog_of_brand(self, sorting):
         '''Функция, которая:
         1. Парсит товар
         2. Проверяет его на повторку
         3. Проверяет наличие бренда (откладывает новые в кэш)
         4. Добавляет в кэш новые объекты цены и продукта, если они прошли проверку на повторки'''
-        for elem in range(1, self.number_of_pages + 1):
-            self.brand_api_url = re.sub(pattern=r'page=\d+\&', repl=f'page={elem}&', string=self.brand_api_url)
-            while True:
-                try:
-                    response = self.scraper.get(self.brand_api_url, headers=self.headers)
-                    json_data = json.loads(response.text)
-                    products_on_page = json_data['data']['products']
-                    break
-                except:
-                    print(elem)
-            for i in range(len(products_on_page)):
-                self.total_products -= 1
-                product_artikul = products_on_page[i]['id'] #специально получаю артикул продукта здесь для того, чтобы передать в функцию проверки на повторки
-                if self.potential_repetitions:
-                    if self.check_repetition_in_catalog(product_artikul): continue
-                seller_artikul = products_on_page[i]['supplierId']
-                seller_name = products_on_page[i]['supplier']
-                seller_object = self.check_seller_existance(seller_artikul, seller_name) #проверка селлера на наличие в БД + откладывание его в кэш (только селлера, тк бренд уже в базе)
-                self.add_new_product(product_in_catalog=products_on_page[i], seller_object=seller_object, product_artikul = product_artikul)
-                if self.total_products == 0: return
-            if self.total_products == 0: return
+        brand_api_url = self.brand_api_url + sorting
+        response = self.scraper.get(brand_api_url, headers=self.headers)
+        json_data = json.loads(response.text)
+        products_on_page = json_data['data']['products']
+        for i in range(len(products_on_page)):
+            self.add_new_product(product_in_catalog=products_on_page[i])
+
+
+
+    def build_top_prods(self):
+        top_builder = TopBuilder(self.dict_brand_products_to_add)
+        print('Вычисляю топ продуктов по цене/отзывам/рейтигу...')
+        self.list_brand_products_to_add_with_scores = list(top_builder.build_top().values())
+        self.list_brand_products_to_add_with_scores = sorted(self.list_brand_products_to_add_with_scores, key=lambda x: x.score)[-20:]
+        if (len(self.list_brand_products_to_add_with_scores)) < 20:
+            print(f'Топ {self.list_brand_products_to_add_with_scores} продуктов бренда:')
+        else:
+            print(f'Топ 20 продуктов бренда:')
+        for product in self.list_brand_products_to_add_with_scores:
+            print(f'Продукт: {product.url}')
+            print(f'Цена: {product.latest_price}')
+            print(f'Рейтинг: {product.rating}')
+            print(f'Количество отзывов: {product.feedbacks}')
+            print(f'Внутренний скор: {product.score}')
+            print()
+        
 
 
 
@@ -86,22 +91,12 @@ class Brand:
     def add_all_to_db(self):
         '''Функция добавления всех изменений в БД атомарной транзакцией'''
         WBBrand.objects.bulk_create([self.brand_object], update_conflicts=True, unique_fields=['wb_id'], update_fields=['name'])
-        WBSeller.objects.bulk_create(self.sellers_to_add, update_conflicts=True, unique_fields=['wb_id'], update_fields=['name'])
-        WBProduct.objects.bulk_create(self.brand_products_to_add, update_conflicts=True, unique_fields=['artikul'], update_fields=['name']) #ссылается не на id а на wb_id добавленного бренда (тк оно уникальное)
-        artikuls_to_add_price = (list(map(lambda x: x.artikul, self.brand_products_to_add))) #вытаскиваем артикулы, которые точно нужно добавить (независимо от процессов)
-        products_to_add_price = list(WBProduct.objects.filter(artikul__in=artikuls_to_add_price).prefetch_related('wbprice_set')) #вытаскиваем из бд продукты, которые несуществуют для этого процесса
-        updated_prices = []
-        for elem in products_to_add_price:
-            if not elem.wbprice_set.exists(): #проверяем, не проставили ли другие процессы цену у этого продукта => продукт уже полностью добавлен другим процессом, и цена не нужна
-                updated_prices.append(WBPrice(price=elem.latest_price,
-                            added_time=timezone.now(),
-                            product=elem))
-        WBPrice.objects.bulk_create(updated_prices) #добавляю элементы одной командой
-        self.brand_products_to_add.extend(self.product_repetitions_list) #опять же, связи добавятся, потому что у этих продуктов есть уникальное поле артикула + 
-        self.preset_object.save()
-        self.preset_object.products.set(self.brand_products_to_add)
-        self.author_object.enabled_connection.add(*self.brand_products_to_add) #many-to-many связь через автора (вставляется сразу все) - обязательно распаковать список
-        self.author_object.save()
+        WBSeller.objects.bulk_create(self.dict_sellers_to_add.values(), update_conflicts=True, unique_fields=['wb_id'], update_fields=['name'])
+        TopWBProduct.objects.bulk_create(self.list_brand_products_to_add_with_scores, update_conflicts=True, unique_fields=['artikul'], update_fields=['name']) #ссылается не на id а на wb_id добавленного бренда (тк оно уникальное)
+        # self.brand_products_to_add.extend(self.product_repetitions_list) #опять же, связи добавятся, потому что у этих продуктов есть уникальное поле артикула + 
+        # self.author_object.enabled_connection.add(*self.brand_products_to_add) #many-to-many связь через автора (вставляется сразу все) - обязательно распаковать список
+        # self.author_object.save()
+
 
 
     def get_brand_artikul_and_siteId(self):
@@ -135,78 +130,34 @@ class Brand:
         '''Построение url для доступа к api каталога бренда с 
         использованием всех фильтров, сортировок, категорий (путей кастомных)'''
         clear_brand_url = re.sub(pattern=r'\#.+', repl='', string=self.brand_url)
-        sorting = 'popular'
         addons = []
         if '?' in clear_brand_url:
             addons = clear_brand_url.split('?')[1]
             addons = addons.split('&')
-            for elem in addons:
-                if 'sort' in elem:
-                    sorting = elem.split('=')[1]
-        category = re.search(r'(brands\/)([a-z\-\d]+\/)([a-z\-]+)?(\?)?(\#)?', clear_brand_url) #выцепляю категорию
-        if category: 
-            try:
-                category_wb_id = (WBCategory.objects.get(url=category.group(3))).wb_id
-                addons.append(f'subject={category_wb_id}')
-            except:
-                addons.append(self.get_custom_links_of_brand()[category.group(3)])
         if addons: addons =f"&{'&'.join(list(filter(lambda x: True if 'page' not in x and 'sort' not in x and 'bid' not in x and 'erid' not in x else False, addons)))}"
         else: addons = ''
-
-        def repl_plus_4(match_obj):
-            number = match_obj.group(1)
-            number_in_str = str((int(number) + 4))
-            return f'fdlvr={number_in_str}'
-        
-        if 'fdlvr' in addons:
-            addons = re.sub(pattern=r'fdlvr\=(?P<number>\d+?)\&', repl=repl_plus_4, string=addons)
-        return f'https://catalog.wb.ru/brands/v2/catalog?ab_testing=false&appType=1&curr=rub&dest={self.author_object.dest_id}&hide_dtype=13&lang=ru&spp=30&uclusters=3&page=1&brand={self.brand_artikul}&sort={sorting}{addons}'
+        #dest по топу товаров беру только на мск
+        return f'https://catalog.wb.ru/brands/v2/catalog?ab_testing=false&appType=1&curr=rub&dest={-1257786}&hide_dtype=13&lang=ru&spp=30&uclusters=3&page=1&brand={self.brand_artikul}&sort='
 
 
 
     def get_total_products_and_name_brand_in_catalog(self):
         '''Получение количества продуктов для парсинга + 
         имя бренда (для создания объекта бренда при его отсутствии в БД)'''
-        response = self.scraper.get(self.brand_api_url, headers=self.headers)
+        brand_api_url = self.brand_api_url + 'popular'
+        response = self.scraper.get(brand_api_url, headers=self.headers)
         json_data = json.loads(response.text)
         try:
             total_products = json_data['data']['total']
             brand_name = json_data['data']['products'][0]['brand']
         except:
-            print('Не найдено ни одного товара бренда (скорее всего они недоступны в вашем регионе)')
+            print('Не найдено ни одного товара бренда (скорее всего они недоступны центральном регионе)')
             self.dest_avaliable = False
             return 0, None
         #точка входа для вопроса пользователю - сколько продуктов нужно взять, если их больше 10
         print(f'Товары обнаружены! Бренд - {brand_name}. Количество - {total_products}')
-        print(f'Доступных слотов для отслеживания продуктов: {self.author_object.slots}')
-        if total_products > 10:
-            while True:
-                print('Введите количество продуктов, которые нужно сохранить (первые N)')
-                try:
-                    custom_total_products = int(input())
-                    if custom_total_products > total_products:
-                        print('Введено число, превышающее количество найденных продуктов, попробуйте снова')
-                        continue
-                    if custom_total_products > self.author_object.slots:
-                        print(f'Недостаточно слотов для отслеживания продуктов. Доступных слотов - {self.author_object.slots}')
-                        continue
-                    break
-                except:
-                    print('Введено некорректное число, попробуйте снова')
-        else:
-            custom_total_products = total_products
-        self.author_object.slots = self.author_object.slots-custom_total_products
-        return custom_total_products, brand_name
-
-
-
-    def get_number_of_pages_in_catalog(self):
-        '''Получение количества страниц для парсинга 
-        плюс настройка ограничения в 100 страниц'''
-        number_of_pages = math.ceil(self.total_products/100)
-        if number_of_pages > 100:
-            number_of_pages = 100
-        return number_of_pages
+        print(f'Выполняется анализ топовых продуктов....')       
+        return total_products, brand_name
     
 
 
@@ -217,66 +168,43 @@ class Brand:
         return WBBrand(wb_id=self.brand_artikul,
                     name=self.brand_name,
                     main_url=f'https://www.wildberries.ru/seller/{self.brand_artikul}')
-    
-
-
-    def build_raw_preset_object(self, raw_brand_url):
-        '''Создание объекта пресета без лишнего обращения в БД (конфликтов нет - заносится в любом случае)'''
-        return WBPreset(name=f'Товары бренда {self.brand_object.name} ({self.total_products} шт.)',
-                        main_url = raw_brand_url,
-                        max_elems = self.total_products,
-                        author_id=self.author_id)
-    
-
-
-    @utils.time_count
-    def get_repetitions_catalog_brand(self):
-        '''Функция взятия всех продуктов + артикулов в БД бренда, 
-        по которому будет производиться парсинг (при его наличии)'''
-        potential_repetitions = []
-        potential_repetitions = WBProduct.objects.filter(brand__wb_id=self.brand_object.wb_id)
-        potential_repetitions = dict(map(lambda x: (x.artikul, x), potential_repetitions))
-        return potential_repetitions
-
-
-
-    def check_repetition_in_catalog(self, product_artikul_to_check):
-        '''Проверка на дубликат продукта в получаемом каталоге'''
-        potential_repetition = self.potential_repetitions.get(product_artikul_to_check)
-        if potential_repetition:
-            self.product_repetitions_list.append(potential_repetition)
-            return True
         
 
 
-    def check_seller_existance(self, seller_artikul, seller_name):
+    def build_raw_seller_object(self, seller_artikul, seller_name):
         '''Проверка продавца на существование в БД + откладывание его в кэш при отсутствии'''
-        if not self.sellers_in_db_dict.get(seller_artikul):
-            seller_object = WBSeller(name=seller_name,
-                        wb_id=seller_artikul,
-                        main_url=f'https://www.wildberries.ru/seller/{seller_artikul}', #баг только с самим вб
-                        full_control = False)
-            self.sellers_to_add.append(seller_object) #добавляем в список для добавления в БД
-            self.sellers_in_db_dict.update({seller_artikul:seller_object})
-        else:
-            seller_object = self.sellers_in_db_dict[seller_artikul]
+        seller_object = WBSeller(name=seller_name,
+                    wb_id=seller_artikul,
+                    main_url=f'https://www.wildberries.ru/seller/{seller_artikul}', #баг только с самим вб
+                    full_control = False)
+        seller_object = self.dict_sellers_to_add.setdefault(seller_artikul, seller_object)
         return seller_object
 
 
 
-    def add_new_product(self, product_in_catalog, seller_object, product_artikul):
+    def add_new_product(self, product_in_catalog):
         '''Сборка объекта продукта и объекта цены + добавление их в кэш'''
-        product_url = f'https://www.wildberries.ru/catalog/{product_artikul}/detail.aspx'
         name = product_in_catalog['name']
         price_element = product_in_catalog['sizes'][0]['price']['product'] // 100
-        new_product = WBProduct(name=name,
+        product_artikul = product_in_catalog['id']
+        product_url = f'https://www.wildberries.ru/catalog/{product_artikul}/detail.aspx'
+        seller_artikul = product_in_catalog['supplierId']
+        rating = product_in_catalog['reviewRating']
+        feedbacks = product_in_catalog['feedbacks']
+        seller_name = product_in_catalog['supplier']
+
+        seller_object = self.build_raw_seller_object(seller_artikul, seller_name)
+        new_product = TopWBProduct(name=name,
                 artikul=product_artikul,
-                latest_price=price_element,
+                latest_price = price_element,
+                rating=rating,
+                feedbacks=feedbacks,
                 wb_cosh=True,
                 url=product_url,
                 brand=self.brand_object,
                 seller=seller_object)
-        self.brand_products_to_add.append(new_product)
+        new_product = {product_artikul: new_product}
+        self.dict_brand_products_to_add.update(new_product)
         
 
 
