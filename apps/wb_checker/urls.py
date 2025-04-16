8466ea5dcd9b543aa455376416c09f5a4341e91d
coldtema <coldtema1703@gmail.com>
2025-04-16 03:29:13 +0300

    deleted unnessesary funcs from template and urls

--- apps/wb_checker/urls.py
+++ apps/wb_checker/urls.py
@@ -4,9 +4,7 @@ from apps.wb_checker import views
 urlpatterns = [
     path('', views.all_price_list, name='all_price_list'),
     path('clear_db/', views.clear_db, name='clear_db'),
-    path('update_categories/', views.update_brands_categories, name='update_categories'),
     path('update_prices/', views.update_prices, name='update_prices'),
     path('update_avaliability/', views.update_avaliability, name='update_avaliability'),
-    path('load_test_data/', views.load_test_data, name='load_test_data'),
     path('update_all_menu_categories/', views.update_menu_categories, name='update_all_menu_categories'),
 ]
