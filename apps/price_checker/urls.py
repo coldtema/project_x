580ed681b3535d70508a41d8dd92e582e270322d
coldtema <coldtema1703@gmail.com>
2025-04-24 23:56:44 +0300

    added new path in urls for sharing product

--- apps/price_checker/urls.py
+++ apps/price_checker/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('update_prices/', views.update_prices, name='update_prices'),
     path('price_history/<int:id>', views.price_history, name='price_history'),
     path('delete_product/<int:id>', views.delete_product, name='delete_product'),
-    path('price_history/delete_price/<int:id>', views.delete_price, name='delete_price')
+    path('price_history/delete_price/<int:id>', views.delete_price, name='delete_price'),
+    path('share_product/<int:id>', views.ShareProduct.as_view(), name='share_product'),
 ]
