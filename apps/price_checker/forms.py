fc96955374a413df6a57fc37acb90bcab54cc6fe
coldtema <coldtema1703@gmail.com>
2025-04-25 23:57:46 +0300

    added search_form

--- apps/price_checker/forms.py
+++ apps/price_checker/forms.py
@@ -12,3 +12,7 @@ class ProductForm(forms.ModelForm):
 class SendMailForm(forms.Form):
     comment = forms.CharField(max_length=1000, widget=forms.Textarea, label='Введите ваш комментарий (можно оставить пустым)', required=False)
     email_to = forms.EmailField(label='Введите email получателя')
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField(max_length=100, label='', help_text='Введите сюда текст поиска')
