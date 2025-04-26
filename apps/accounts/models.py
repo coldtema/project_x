c400069e530d203b62f06313c081f0c66b70b268
coldtema <coldtema1703@gmail.com>
2025-04-26 23:03:49 +0300

    added custom user form in accounts

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    class Subscription(models.TextChoices):
+        GOLD = 'GOLD', 'Golden Status'
+        SILVER = 'SILVER', 'Silver Status'
+        BRONZE = 'BRONZE', 'Bronze Status'
+        FREE = 'FREE', 'Free Status'
+    email = models.EmailField(unique=True)
+    subscription = models.CharField(max_length=6, default=Subscription.FREE, choices=Subscription.choices)
+    slots = models.IntegerField(default=1000)
+    dest_name = models.CharField(max_length=1000, null=True)
+    dest_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.username
