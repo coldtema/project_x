0b31da2827436d761f09086750a264d7caeac9f0
coldtema <coldtema1703@gmail.com>
2025-04-26 23:05:02 +0300

    added signup view in accounts

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-from django.views import View
+from django.views.generic import CreateView
+from .forms import SignUpForm
+from django.urls import reverse_lazy
 
-
+class SignUpView(CreateView):
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+    form_class = SignUpForm
