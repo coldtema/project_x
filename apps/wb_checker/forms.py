aca62c93b1e24c7e935e8c93e6c4cc4ff41afb61
coldtema <coldtema1703@gmail.com>
2025-04-27 20:21:33 +0300

    deleten unnessesary instances

--- apps/wb_checker/forms.py
+++ apps/wb_checker/forms.py
@@ -1,13 +1,7 @@
 from django import forms
 
-ACTION_TYPE=(('product', 'product'), 
-             ('brand', 'brand'), 
-             ('seller', 'seller'),
-             ('promo', 'promo'))
-
 class WBProductForm(forms.Form):
     url = forms.CharField(max_length=1000, required=True)
-    # action_type = forms.ChoiceField(choices=ACTION_TYPE)
 
 
 class WBDestForm(forms.Form):
