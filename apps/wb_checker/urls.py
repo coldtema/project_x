10f0220434627820a2336aff99e1960880ee9b01
coldtema <coldtema1703@gmail.com>
2025-04-29 23:20:02 +0300

    added path for recomendations in urls of wb_checker

--- apps/wb_checker/urls.py
+++ apps/wb_checker/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('update_all_menu_categories/', views.update_menu_categories, name='update_all_menu_categories'),
     path('update_top_prods/', views.update_top_prods, name='update_top_prods'),
     path('update_top_prods_info/', views.update_top_prods_info, name='update_top_prods_info'),
+    path('recommendations/', views.RecommentationsList.as_view(), name='recommendations'),
 ]
