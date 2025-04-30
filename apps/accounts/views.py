3a9ad38d44baa6ad7a8541cfdc96de9aa9ee7de3
coldtema <coldtema1703@gmail.com>
2025-04-30 03:16:14 +0300

    added profile viewfunc in accounts app

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from .forms import SignUpForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 class SignUpView(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
     form_class = SignUpForm
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
