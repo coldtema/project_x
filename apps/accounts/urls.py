7968d78eaa64f682cd8eb3d7494597e2b05fc1ee
coldtema <coldtema1703@gmail.com>
2025-04-30 03:15:30 +0300

    added profile url in accounts app

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SignUpView
+from apps.accounts import views
 
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('profile/', views.profile, name='profile')
 ]
