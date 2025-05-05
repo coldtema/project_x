050a249a4e02d48127c12cfd627f07f6b9712736
coldtema <coldtema1703@gmail.com>
2025-05-05 12:46:30 +0300

    adden login for view

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'core/index.html')
 
 
-
+@login_required
 def menu(request):
     return render(request, 'core/menu.html')
