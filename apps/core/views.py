511fcd5d215c629a8aaaed63dd56013028838ff2
coldtema <coldtema1703@gmail.com>
2025-05-07 02:36:53 +0300

    added menu view class in core

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from apps.price_checker.models import Product
+from apps.accounts.models import CustomUser
+
 
 def index(request):
     return render(request, 'core/index.html')
 
 
-@login_required
-def menu(request):
-    return render(request, 'core/menu.html')
+# @login_required
+# def menu(request):
+#     return render(request, 'core/menu.html')
+
+
+class MenuView(LoginRequiredMixin, View):
+    def get(self, request):
+        prods_snippet = request.user.product_set.all()[:3]
+        print(prods_snippet)
+        return render(request, 'core/menu.html', context={'prods_snippet': prods_snippet})
