38b06369de8140906fbd10b461f8bb8cab2582e6
coldtema <coldtema1703@gmail.com>
2025-05-06 17:56:56 +0300

    added attrs in forms and made widget

--- apps/price_checker/forms.py
+++ apps/price_checker/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Product
 
+
+front_attrs = {'class': 'w-full px-3 py-2 rounded-md border-2 border-gray-300 dark:border-gray-600 bg-white dark:bg-neutral-700 text-gray-900 dark:text-white placeholder-gray-400 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500'}
+
 class ProductForm(forms.ModelForm):
-    url = forms.URLField(label='Вот сюда ссылку ->')
+    url = forms.URLField(label='Вот сюда ссылку ->', help_text='Введите ссылку на товар', widget=forms.TextInput(attrs=front_attrs))
 
     class Meta:
         model = Product
@@ -15,4 +18,4 @@ class SendMailForm(forms.Form):
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(max_length=100, label='', help_text='Введите сюда текст поиска')
+    query = forms.CharField(max_length=100, label='', help_text='Введите сюда текст поиска', widget=forms.TextInput(attrs=front_attrs))
