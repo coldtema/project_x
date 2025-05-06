4a5597ef1c9242e46e6af01dd1a1c269bef56f6f
coldtema <coldtema1703@gmail.com>
2025-05-06 18:00:08 +0300

    added attrs in forms and made widget

--- apps/wb_checker/forms.py
+++ apps/wb_checker/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 
+
+front_attrs = {'class': 'w-full px-3 py-2 rounded-md border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-700 text-gray-900 dark:text-white placeholder-gray-400 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500'}
+
 class WBProductForm(forms.Form):
-    url = forms.CharField(max_length=1000, required=True)
+    url = forms.CharField(max_length=1000, required=True, widget=forms.TextInput(attrs=front_attrs))
 
 
 class WBDestForm(forms.Form):
-    address = forms.CharField(max_length=1000)
+    address = forms.CharField(max_length=1000, widget=forms.TextInput(attrs=front_attrs))
