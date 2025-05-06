c67eea9a20ff3711611489c3a43e9fdc6f3ecbaa
coldtema <coldtema1703@gmail.com>
2025-05-07 02:33:11 +0300

    added many settings urls to the acoount app urls

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,5 +6,9 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('profile_edit/', views.profile_edit, name='profile_edit'),
+    path('subscription_edit/', views.subscription_edit, name='subscription_edit'),
+    path('geolocation_edit/', views.GeolocationEditView.as_view(), name='geolocation_edit'),
+    path('change_password/', views.change_password, name='change_password'),
 ]
