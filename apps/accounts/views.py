b0f7beaa0fe51dd12da1a9f04d994dd861f4d13f
coldtema <coldtema1703@gmail.com>
2025-05-08 01:22:48 +0300

    added user slots for profile context in view func

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from .models import CustomUser
+from apps.price_checker.models import Shop, Tag
 from django.views import View
 from .forms import SignUpForm
 from django.urls import reverse_lazy
@@ -8,6 +9,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import WBDestForm
 from .pickpoints import load_dest_to_author
+from django.db.models import F, IntegerField, ExpressionWrapper
+from django.db.models.aggregates import Sum
 
 class SignUpView(CreateView):
     success_url = reverse_lazy('login')
@@ -16,7 +19,11 @@ class SignUpView(CreateView):
 
 @login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    sub_dict = {'FREE': 10,
+                'PLATINUM':100,
+                'ULTIMA':1000}
+    used_slots = sub_dict[request.user.subscription] - request.user.slots
+    return render(request, 'accounts/profile.html', context={'used_slots':used_slots})
 
 
 def update_discount_balance(request):
