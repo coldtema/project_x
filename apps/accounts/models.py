3b0e02207190d283732e565344917eeb8557e6e2
coldtema <coldtema1703@gmail.com>
2025-05-08 01:20:20 +0300

    added custom discount balance in cusom user + changed subscription plans

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     class Subscription(models.TextChoices):
-        GOLD = 'GOLD', 'Golden Status'
-        SILVER = 'SILVER', 'Silver Status'
-        BRONZE = 'BRONZE', 'Bronze Status'
+        ULTIMA = 'ULTIMA', 'Ultima Status'
+        PLATINUM = 'PLATINUM', 'Platinum Status'
         FREE = 'FREE', 'Free Status'
     email = models.EmailField(unique=True)
-    subscription = models.CharField(max_length=6, default=Subscription.FREE, choices=Subscription.choices)
-    slots = models.IntegerField(default=1000)
+    subscription = models.CharField(max_length=8, default=Subscription.FREE, choices=Subscription.choices)
+    slots = models.IntegerField(default=10)
     dest_name = models.CharField(max_length=1000, default="Москва")
     dest_id = models.CharField(max_length=100, default="-1257786")
+    discount_balance = models.IntegerField(default=0)
 
     def __str__(self):
         return self.username
