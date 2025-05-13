43534caa6d2c95c1b7023fe16d6fcbd32c63f13d
coldtema <coldtema1703@gmail.com>
2025-05-13 23:45:38 +0300

    added template tag for abs

--- apps/core/templatetags/custom_math.py
+++ apps/core/templatetags/custom_math.py
@@ -1,7 +1,13 @@
 from django import template
+import math
 
 register = template.Library()
 
 @register.filter
 def minus(value, arg):
     return int(value) - int(arg)
+
+
+@register.filter
+def abs_tag(value):
+    return abs(value)
