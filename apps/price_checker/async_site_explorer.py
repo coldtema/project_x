11743d386f7ff99768e60fc68919356a981448eb
coldtema <coldtema1703@gmail.com>
2025-05-22 21:15:58 +0300

    bug fix danielonline

--- apps/price_checker/async_site_explorer.py
+++ apps/price_checker/async_site_explorer.py
@@ -666,10 +666,10 @@ class Parser:
         '''Функция для парсинга товара из danielonline'''
         response = await self.client.get(product_url)
         soup_engine = BeautifulSoup(response.text, 'lxml')
-        price_element = soup_engine.find('div', class_='item-price__value').text.strip()
-        price_element = price_element.split('руб.')[0]
+        all_info = soup_engine.find('title').text.strip()
+        price_element = re.search(pattern=r'по\sцене\s(.+?)(руб\.)', string=all_info).group(1)
         price_element = int(''.join(list(filter(lambda x: True if x.isdigit() else False, price_element))))
-        name = soup_engine.find('h1').text.strip()
+        name = re.search(pattern=r'(.+?)\,', string=all_info).group(1)
         return {'price_element': price_element, 'name': name, 'shop': 'danielonline'}
 
 
