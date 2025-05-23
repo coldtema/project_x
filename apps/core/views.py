9c32dfcfbd89ebdf043ef83cd58ea4ba59e01c9c
Artem <coldtema1703@gmail.com>
2025-05-23 14:47:47 +0300

    faked post in index (#)

--- apps/core/views.py
+++ apps/core/views.py
@@ -4,10 +4,16 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from apps.price_checker.models import Product
 from apps.accounts.models import CustomUser
+from datetime import date
 
 
 def index(request):
-    return render(request, 'core/index.html')
+    post = {'title':'first post', 
+            'date': date(year=2002, month=9, day=25),
+            'summary': 'text of post',
+            'image_url': 'http://avatars.mds.yandex.net/get-vthumb/2044023/a3932f1604e45342599fd8f71f767935/800x450',
+            'image': '1'}
+    return render(request, 'core/index.html', context={'posts':[post]})
 
 
 # @login_required
@@ -33,3 +39,7 @@ class MenuView(LoginRequiredMixin, View):
         recs_snippet = sorted(prods, key=lambda x: x.true_discount)[-5:]
         return render(request, 'core/menu.html', context={'prods_snippet': prods_snippet,
                                                           'recs_snippet': recs_snippet})
+    
+
+def faq(request):
+    return render(request, 'core/faq.html')
