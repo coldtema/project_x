ade8a20d03a0268f445cfb86d51a30a8abda2b77
Artem <coldtema1703@gmail.com>
2025-05-26 14:57:55 +0300

    added urls for contacts template

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -7,5 +7,6 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuView.as_view(), name='menu'),
-    path('faq', views.faq, name='faq')
+    path('faq/', views.faq, name='faq'),
+    path('contacts/', views.contacts, name='contacts')
 ]
