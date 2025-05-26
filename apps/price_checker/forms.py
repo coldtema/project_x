19d8c03355603a1f71f426f83e851ada66a1cbce
Artem <coldtema1703@gmail.com>
2025-05-26 14:59:45 +0300

    fixed style of form in price_checker

--- apps/price_checker/forms.py
+++ apps/price_checker/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Product
 
 
-front_attrs = {'class': 'w-full px-3 py-2 rounded-md border-2 border-gray-300 dark:border-gray-600 bg-white dark:bg-neutral-700 text-gray-900 dark:text-white placeholder-gray-400 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500'}
+front_attrs = {'class': 'w-full px-3 py-2 rounded-md border-2 border-gray-300 dark:border-gray-600 bg-gray-100 dark:bg-neutral-700 text-gray-900 dark:text-white placeholder-gray-400 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500'}
 
 class ProductForm(forms.ModelForm):
     url = forms.URLField(label='Вот сюда ссылку ->', help_text='Введите ссылку на товар', widget=forms.TextInput(attrs=front_attrs))
