dfee35a2cd224691fe432bdf164863598cfa2d8b
coldtema <coldtema1703@gmail.com>
2025-06-02 20:03:21 +0300

    registered wbbrand in admin panel

--- apps/wb_checker/admin.py
+++ apps/wb_checker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WBBrand, WBSeller, WBMenuCategory, WBProduct
 
-# Register your models here.
+@admin.register(WBBrand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
+    ordering = ['name']
