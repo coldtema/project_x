84c1224cf6862da2182ba4e1595141d37cb5166e
coldtema <coldtema1703@gmail.com>
2025-06-07 21:19:38 +0300

    added fields in custom user model for separating notifications + migration

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -13,6 +13,8 @@ class CustomUser(AbstractUser):
     dest_name = models.CharField(max_length=1000, default="Москва")
     dest_id = models.CharField(max_length=100, default="-1257786")
     discount_balance = models.IntegerField(default=0)
+    notification_discount_price = models.IntegerField(default=100, verbose_name='Скидка для уведомления (руб)')
+    notification_discount = models.IntegerField(default=10, verbose_name='Скидка для уведомления (%)')
 
     def __str__(self):
         return self.username
