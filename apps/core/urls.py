b0eb84f16dadce77e1d231f6dd6cf4bd57742db1
coldtema <coldtema1703@gmail.com>
2025-06-08 21:57:36 +0300

    added url path for deleting notification

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuView.as_view(), name='menu'),
     path('faq/', views.faq, name='faq'),
-    path('contacts/', views.contacts, name='contacts')
+    path('delete_notification/<int:id>', views.delete_notification, name='delete_notification'),
+    path('notifications_swap/', views.notifications_swap, name='notifications_swap')
 ]
