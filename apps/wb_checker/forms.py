f402dbb13c6a4369bda7cb3120ab816b5fb07974
coldtema <coldtema1703@gmail.com>
2025-06-18 02:07:23 +0300

    added help_text in wb_form

--- apps/wb_checker/forms.py
+++ apps/wb_checker/forms.py
@@ -4,8 +4,8 @@ from django import forms
 front_attrs = {'class': 'w-full px-3 py-2 rounded-md border-2 border-gray-300 dark:border-gray-600 bg-gray-100 dark:bg-neutral-700 text-gray-900 dark:text-white placeholder-gray-400 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500'}
 
 class WBProductForm(forms.Form):
-    url = forms.CharField(max_length=1000, required=True, widget=forms.TextInput(attrs=front_attrs))
-    
+    url = forms.CharField(max_length=1000, required=True, help_text='Введите ссылку/артикул на товар с Wildberries', widget=forms.TextInput(attrs=front_attrs))
+
 
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100, label='', help_text='Введите сюда текст поиска', widget=forms.TextInput(attrs=front_attrs))
