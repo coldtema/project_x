91c7802259b4f1d560d1f6590c64d33ab832b19d
coldtema <coldtema1703@gmail.com>
2025-06-18 18:47:54 +0300

    added urls for resetting password in accounts app

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -6,9 +6,12 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('profile/', views.profile, name='profile'),
     path('notification_edit/', views.notification_edit, name='notification_edit'),
     path('subscription_edit/', views.subscription_edit, name='subscription_edit'),
     path('geolocation_edit/', views.GeolocationEditView.as_view(), name='geolocation_edit'),
-    path('change_password/', views.change_password, name='change_password'),
 ]
