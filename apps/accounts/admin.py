971ef56c2af792349c71488c9660cda77a689108
coldtema <coldtema1703@gmail.com>
2025-06-20 17:55:03 +0300

    registeresd customuser and subrequest in admin panel

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import SubRequest, CustomUser
 
-# Register your models here.
+@admin.register(SubRequest)
+class SubRequestAdmin(admin.ModelAdmin):
+    list_display = ['price', 'duration', 'sub_plan', 'user', 'status', 'created']
+    ordering = ['-created']
+    
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display =  ['email', 'subscription', 'sub_expire', 'slots', 'dest_name', 'dest_id', 'discount_balance', 'notification_discount_price', 'notification_discount', 'pricedown_notification', 'priceup_notification']
+    ordering = ['-date_joined']
