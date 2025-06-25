f2618d04942871e3e564662a89f1e151ec68513b
coldtema <coldtema1703@gmail.com>
2025-06-25 23:06:56 +0300

    sa

--- project_x/middleware.py
+++ project_x/middleware.py
@@ -1,6 +1,6 @@
 def simple_middleware(get_response):
     def middleware(request):
         print("is_secure:", request.is_secure())
-        print("X-Forwarded-Proto:", request.META.get("HTTP_X_FORWARDED_PROTO"))
+        print(request.META)
         return get_response(request)
     return middleware
