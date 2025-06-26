86b70e0b094890944b1ab69f744045a53c173374
coldtema <coldtema1703@gmail.com>
2025-06-26 03:22:43 +0300

    resetted admin classes in accounts app

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -3,7 +3,7 @@ from .models import SubRequest, CustomUser
 
 @admin.register(SubRequest)
 class SubRequestAdmin(admin.ModelAdmin):
-    list_display = ['price', 'duration', 'sub_plan', 'user', 'status', 'created']
+    list_display = ['user', 'price', 'duration', 'sub_plan', 'status', 'created']
     ordering = ['-created']
     
 
@@ -11,4 +11,5 @@ class SubRequestAdmin(admin.ModelAdmin):
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display =  ['email', 'subscription', 'sub_expire', 'slots', 'dest_name', 'dest_id', 'discount_balance', 'notification_discount_price', 'notification_discount', 'pricedown_notification', 'priceup_notification']
+    list_filter = ['subscription']
     ordering = ['-date_joined']
