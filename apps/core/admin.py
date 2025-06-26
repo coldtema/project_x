27d2e79c84521ce642c698d5f42f8ccf1d042a14
coldtema <coldtema1703@gmail.com>
2025-06-26 03:22:51 +0300

    resetted admin classes in core app

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Notification
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'text', 'date']
+
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'time', 'additional_link']
+    list_filter = ['user']
+    ordering = ['-time']
