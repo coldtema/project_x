e73d64baaf5701bb30b874989f0a397eca492205
coldtema <coldtema1703@gmail.com>
2025-07-09 17:08:27 +0300

    registered new model in admin panel

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SubRequest, CustomUser
+from .models import SubRequest, CustomUser, TelegramUser
 
 @admin.register(SubRequest)
 class SubRequestAdmin(admin.ModelAdmin):
@@ -10,6 +10,10 @@ class SubRequestAdmin(admin.ModelAdmin):
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display =  ['email', 'subscription', 'sub_expire', 'slots', 'dest_name', 'dest_id', 'discount_balance', 'notification_discount_price', 'notification_discount', 'pricedown_notification', 'priceup_notification']
+    list_display =  ['email', 'subscription', 'sub_expire', 'slots', 'dest_name', 'dest_id', 'discount_balance', 'notification_discount_price', 'notification_discount', 'pricedown_notification', 'priceup_notification', 'tg_user', 'tg_token']
     list_filter = ['subscription']
     ordering = ['-date_joined']
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display =  ['username', 'first_name', 'tg_id']
