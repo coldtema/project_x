e59b73e297768fdf56ff07ee8488be8c556f16b7
coldtema <coldtema1703@gmail.com>
2025-07-09 16:32:42 +0300

    //

--- apps/core/bot.py
+++ apps/core/bot.py
@@ -8,10 +8,10 @@ def send_first_telegram_message(chat_id):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
         'chat_id': chat_id,
-        'text': ('Привет! Это бот для отправки уведомлений от сайта <a href="https://heavydrop.ru"><b>HeavyDrop</b></a>!.\n',
-                 'Усли ты еще не зарегистрирован, то можешь это сделать по ссылке <a href="https://heavydrop.ru/accounts/signup/">Регистрация</a>.\n',
-                 'Если ты уже зарегистрирован, то можешь перейти во вкладку <a href="https://heavydrop.ru/accounts/notification_edit/">Уведомления</a> и получить код доступа для отправки уведомлений через Telegram.\n',
-                 'Если у тебя есть какие-то вопросы, то можешь написать в <a href=" https://heavydrop.ru/contacts">Тех. поддержку</a>.\n',
+        'text': ('Привет! Это бот для отправки уведомлений от сайта <a href="https://heavydrop.ru"><b>HeavyDrop</b></a>!\n'
+                 'Усли ты еще не зарегистрирован, то можешь это сделать по ссылке <a href="https://heavydrop.ru/accounts/signup/">Регистрация</a>.\n'
+                 'Если ты уже зарегистрирован, то можешь перейти во вкладку <a href="https://heavydrop.ru/accounts/notification_edit/">Уведомления</a> и получить код доступа для отправки уведомлений через Telegram.\n'
+                 'Если у тебя есть какие-то вопросы, то можешь написать в <a href=" https://heavydrop.ru/contacts">Тех. поддержку</a>.\n'
 ),
         'parse_mode': 'HTML'
     }
