c85b4cd602ecf6d1370f54806872fab76fae88e2
coldtema <coldtema1703@gmail.com>
2025-07-10 22:16:39 +0300

    added sending messages on commands

--- apps/core/bot.py
+++ apps/core/bot.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+import random
 
 
 
@@ -46,7 +47,7 @@ def send_message_to_paste_code(chat_id):
     }
     payload = {
         'chat_id': chat_id,
-        'text': ('Жду код для привязки вашего профиля! \n\n (6 цифр без пробелов и дополнительных знаков)'),
+        'text': ('Жду код для привязки вашего профиля!\n\n(6 цифр без пробелов и дополнительных знаков)'),
         'parse_mode': 'HTML',
         "reply_markup": json.dumps(keyboard)
     }
@@ -121,7 +122,82 @@ def send_message_of_deleting_connection(chat_id):
     }
     payload = {
         'chat_id': chat_id,
-        'text': ('Вы успешно отписались от уведомлений через Telegram.\n\n Если Вам не понравилось пользоваться сервисом или у вас есть какие-либо вопросы, то обязательно напишите нам в <a href="https://heavydrop.ru/accounts/notification_edit/">поддержку</a>.'),
+        'text': ('Вы успешно отписались от уведомлений через Telegram.\n\nЕсли Вам не понравилось пользоваться сервисом или у вас есть какие-либо вопросы, то обязательно напишите нам в <a href="https://heavydrop.ru/accounts/notification_edit/">поддержку</a>.'),
+        'parse_mode': 'HTML',
+        "reply_markup": json.dumps(keyboard)
+    }
+
+    response = requests.post(url, data=payload)
+    print("Status:", response.status_code)
+    print("Response:", response.text)
+
+
+
+
+def send_support_message(chat_id):
+    token = os.getenv('BOT_API_KEY')
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    keyboard = {
+        "keyboard": [
+            [{"text": "🔔 Вставить код"}],
+            [{"text": "💬 Поддержка"}]
+        ],
+        "resize_keyboard": True,
+        "one_time_keyboard": False
+    }
+    payload = {
+        'chat_id': chat_id,
+        'text': ('Контакты\n\nE-mail:<a href="mailto:hello@heavydrop.ru" heavydrop.ru@gmail.com</a>\nTelegram of admin: <a href="https://t.me/coldtema" @coldtema</a>\n\nМы ответим в течение 24 часов в рабочие дни.'),
+        'parse_mode': 'HTML',
+        "reply_markup": json.dumps(keyboard)
+    }
+
+    response = requests.post(url, data=payload)
+    print("Status:", response.status_code)
+    print("Response:", response.text)
+
+
+
+
+def send_unknown_message(chat_id):
+    token = os.getenv('BOT_API_KEY')
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    keyboard = {
+        "keyboard": [
+            [{"text": "🔔 Вставить код"}],
+            [{"text": "💬 Поддержка"}]
+        ],
+        "resize_keyboard": True,
+        "one_time_keyboard": False
+    }
+    payload = {
+        'chat_id': chat_id,
+        'text': ('Я пока не знаю, что Вам на это ответить...'),
+        'parse_mode': 'HTML',
+        "reply_markup": json.dumps(keyboard)
+    }
+
+    response = requests.post(url, data=payload)
+    print("Status:", response.status_code)
+    print("Response:", response.text)
+
+
+
+def send_52_message(chat_id):
+    token = os.getenv('BOT_API_KEY')
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    fifty_two_list = os.getenv('fifty_two_list').split('\n')
+    keyboard = {
+        "keyboard": [
+            [{"text": "🔔 Вставить код"}],
+            [{"text": "💬 Поддержка"}]
+        ],
+        "resize_keyboard": True,
+        "one_time_keyboard": False
+    }
+    payload = {
+        'chat_id': chat_id,
+        'text': fifty_two_list[random.randint(0, len(fifty_two_list)-1)],
         'parse_mode': 'HTML',
         "reply_markup": json.dumps(keyboard)
     }
