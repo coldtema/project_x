06c0c234bac34ca03e5f4d08df81aa1465da2fd7
coldtema <coldtema1703@gmail.com>
2025-07-10 16:51:04 +0300

    added new url paths in accounts app

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -13,8 +13,9 @@ urlpatterns = [
     path('reset/done/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('profile/', views.profile, name='profile'),
     path('notification_edit/', views.notification_edit, name='notification_edit'),
+    path('notification_edit/make_tg_code/', views.make_tg_code, name='make_tg_code'),
     path('subscription_edit/', views.subscription_edit, name='subscription_edit'),
     path('geolocation_edit/', views.GeolocationEditView.as_view(), name='geolocation_edit'),
     path('subscription_edit/payment/', views.payment, name='payment'),
-    path('subscription_edit/payment_history/', views.payment_history, name='payment_history')
+    path('subscription_edit/payment_history/', views.payment_history, name='payment_history'),
 ]
