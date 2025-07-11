2c8ff7addc5ddcd58fa27528f7f3dd29f04a6a02
coldtema <coldtema1703@gmail.com>
2025-07-11 20:20:56 +0300

    added tg_text field in notif

--- apps/core/models.py
+++ apps/core/models.py
@@ -5,11 +5,13 @@ from apps.price_checker.models import Product
 
 class Notification(models.Model):
     text = models.CharField(max_length=512, verbose_name='Текст уведомления')
+    tg_text = models.CharField(max_length=512, verbose_name='Текст телеграм-уведомления', default='')
     time = models.DateTimeField(auto_now_add = True, verbose_name='Время уведомления')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, verbose_name='Обычный продукт')
     wb_product = models.ForeignKey(WBDetailedInfo, on_delete=models.CASCADE, null=True, verbose_name='WB продукт')
     additional_link = models.URLField(null = True, verbose_name='Доп. ссылка')
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, verbose_name='Пользователь')
+
 
 
 class Post(models.Model):
